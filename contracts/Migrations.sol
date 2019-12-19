d31e968d89ff3d919a3f244b1e930a0a8a273b0d
Jonathan Wolff <jwolff@isentropy.com>
2019-12-19 12:27:28 +0100

    Migrations uses Ownable

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,22 +1,17 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-contract Migrations {
-  address public owner;
+import "./Ownable.sol";
+
+contract Migrations is Ownable{
   uint public last_completed_migration;
 
-  constructor() public {
-    owner = msg.sender;
-  }
+  constructor() public Ownable(msg.sender) {}
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
-  function setCompleted(uint completed) public restricted {
+  function setCompleted(uint completed) public onlyOwner {
     last_completed_migration = completed;
   }
 
-  function upgrade(address new_address) public restricted {
+  function upgrade(address new_address) public onlyOwner {
     Migrations upgraded = Migrations(new_address);
     upgraded.setCompleted(last_completed_migration);
   }
