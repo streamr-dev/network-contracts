9d9037cb5cbae6891f741a959a37d19316487e9a
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2021-04-16 15:34:55 +0200

    continue streamregistry contract implementation

--- contracts/StreamRegistry/StreamRegistry.sol
+++ contracts/StreamRegistry/StreamRegistry.sol
@@ -5,9 +5,12 @@ contract StreamRegistry {
     event TransferedViewRights(uint streamid, address from, address to, uint8 amount);
     event TransferedPublishRights(uint streamid, address from, address to, uint8 amount);
 
+    enum PermissionType { Edit, Delete, Publish, Subscribe, Share }
+
+    mapping (string => uint32) private streamIdToVersion;
     mapping (string => string) public streamIdToMetadata;
-    // streamid ->  useraddr -> permissions struct 
-    mapping (string => mapping(address => Permission)) public streamIdToPermissions;
+    // streamid ->  keccak256(version, useraddress); -> permissions struct 
+    mapping (string => mapping(bytes32 => Permission)) public streamIdToPermissions;
 
     struct Permission {
         bool edit;
@@ -24,49 +27,156 @@ contract StreamRegistry {
     //     _;
     // }
 
-    modifier canShare(string calldata id) {
-        require(streamIdToPermissions[id][msg.sender].share, "no share permission"); //||
+    modifier canShare(string calldata streamId) {
+        require(streamIdToPermissions[streamId][getAddressKey(streamId, msg.sender)].share, "no share permission"); //||
         //streamIdToPermissions[id][msg.sender].edit, "no edit permission");
         _;
     }
-    modifier canDelete(string calldata id) {
-        require(streamIdToPermissions[id][msg.sender].canDelete, "no delete permission"); //||
+    modifier canDelete(string calldata streamId) {
+        require(streamIdToPermissions[streamId][getAddressKey(streamId, msg.sender)].canDelete, "no delete permission"); //||
         //streamIdToPermissions[id][msg.sender].edit, "no edit permission");
         _;
     }
-    modifier canEdit(string calldata id) {
-        require(streamIdToPermissions[id][msg.sender].edit, "no edit permission"); //||
+    modifier canEdit(string calldata streamId) {
+        require(streamIdToPermissions[streamId][getAddressKey(streamId, msg.sender)].edit, "no edit permission"); //||
         //streamIdToPermissions[id][msg.sender].edit, "no edit permission");
         _;
     }
-    modifier itemExists(string calldata id) {
+    modifier streamExists(string calldata streamId) {
         // TODO can stream exist without metadata?
-        require(bytes(streamIdToMetadata[id]).length != 0, "item doesn' exist");
+        require(bytes(streamIdToMetadata[streamId]).length != 0, "stream doesn' exist");
         _;
     }
 
     // TODO do we need an external id or increment ourselves?
     function createStream(string memory streamIdPath, string calldata metadataJsonString) public {
-        // require(bytes(streamIdToMetadata[id]).length == 0, "item id alreay exists!");
         string memory ownerstring = addressToString(msg.sender);
-        ownerstring = string(abi.encodePacked(ownerstring, streamIdPath));
-        streamIdToMetadata[ownerstring] = metadataJsonString;
-        // streamIdToPermissions[rollingId][msg.sender] = 
-        // Permission({
-        //     isAdmin: true,
-        //     publishRights: 1,
-        //     viewRights: 1,
-        //     expirationTime: 0
-        // });
-        emit StreamCreated(ownerstring, metadataJsonString);
+        string memory streamId = string(abi.encodePacked(ownerstring, streamIdPath));
+        require(bytes(streamIdToMetadata[streamId]).length == 0, "item id alreay exists!");
+        streamIdToVersion[streamId] = streamIdToVersion[streamId] + 1;
+        streamIdToMetadata[streamId] = metadataJsonString;
+        streamIdToPermissions[streamId][getAddressKey(streamId, msg.sender)] = 
+        Permission({
+            edit: true,
+            canDelete: true,
+            publish: true,
+            subscribed: true,
+            share: true
+        });
+        emit StreamCreated(streamId, metadataJsonString);
     }
 
-    function editItem(string calldata id, string calldata desc) public itemExists(id) canEdit(id) {
-        streamIdToMetadata[id] = desc;
+    function getAddressKey(string memory streamId, address user) public view returns (bytes32) {
+        return keccak256(abi.encode(streamIdToVersion[streamId], user));
     }
 
-    function getDescription(string calldata id) public view itemExists(id) returns (string memory des) {
-        return streamIdToMetadata[id];
+    function updateStreamMetadata(string calldata streamId, string calldata metadata) public streamExists(streamId) canEdit(streamId) {
+        streamIdToMetadata[streamId] = metadata;
+    }
+
+    function getStreamMetadata(string calldata streamId) public view streamExists(streamId) returns (string memory des) {
+        return streamIdToMetadata[streamId];
+    }
+
+    function deleteStream(string calldata streamId) public streamExists(streamId) canDelete(streamId) {
+        delete streamIdToMetadata[streamId];
+        // streamIdToPermissions[id] = [];
+    }
+
+    
+    function getPermissionsForUser(string calldata streamId, address user) public view returns (Permission memory permission) {
+    // function grantPermissions(uint id, address user, bool[] memory _permission) public itemExists(id) canGrant(id) {
+        // permissions[id][user] = _permission;
+        return streamIdToPermissions[streamId][getAddressKey(streamId, user)];
+    }
+
+    function setPermissionForUser(string calldata streamId, address user, bool edit, 
+        bool canDelete, bool publish, bool subscribe, bool share) public {
+            streamIdToPermissions[streamId][getAddressKey(streamId, user)] = Permission({
+                edit: edit,
+                canDelete: canDelete,
+                publish: publish,
+                subscribed: subscribe,
+                share: share
+           });
+    }
+
+    function revokePermissionForUser(string calldata streamId, address user) public canShare(streamId){
+    // function grantPermissions(uint id, address user, bool[] memory _permission) public itemExists(id) canGrant(id) {
+        // permissions[id][user] = _permission;
+        delete streamIdToPermissions[streamId][getAddressKey(streamId, user)];
+    }
+
+    function hasPermission(string calldata streamId, address user, PermissionType permissionType) public view returns (bool userHasPermission) {
+        if (permissionType == PermissionType.Edit) {
+            return streamIdToPermissions[streamId][getAddressKey(streamId, user)].edit;
+        }
+        else if (permissionType == PermissionType.Delete) {
+            return streamIdToPermissions[streamId][getAddressKey(streamId, user)].canDelete;
+        }
+        else if (permissionType == PermissionType.Subscribe) {
+            return streamIdToPermissions[streamId][getAddressKey(streamId, user)].subscribed;
+        }
+        else if (permissionType == PermissionType.Publish) {
+            return streamIdToPermissions[streamId][getAddressKey(streamId, user)].publish;
+        }
+        else if (permissionType == PermissionType.Share) {
+            return streamIdToPermissions[streamId][getAddressKey(streamId, user)].share;
+        }
+    }
+
+    function grantPermission(string calldata streamId, address user, PermissionType permissionType) public canShare(streamId) {
+        setPermission(streamId, user, permissionType, true);
+    }
+
+    function revokePermission(string calldata streamId, address user, PermissionType permissionType) public canShare(streamId) {
+        setPermission(streamId, user, permissionType, false);
+    }
+
+    function setPermission(string calldata streamId, address user, PermissionType permissionType, bool grant) internal {
+        if (permissionType == PermissionType.Edit) {
+            streamIdToPermissions[streamId][getAddressKey(streamId, user)].edit = grant;
+        }
+        else if (permissionType == PermissionType.Delete) {
+            streamIdToPermissions[streamId][getAddressKey(streamId, user)].canDelete = grant;
+        }
+        else if (permissionType == PermissionType.Subscribe) {
+            streamIdToPermissions[streamId][getAddressKey(streamId, user)].subscribed = grant;
+        }
+        else if (permissionType == PermissionType.Publish) {
+            streamIdToPermissions[streamId][getAddressKey(streamId, user)].publish = grant;
+        }
+        else if (permissionType == PermissionType.Share) {
+            streamIdToPermissions[streamId][getAddressKey(streamId, user)].share = grant;
+        }
+    }
+    
+    // function hasPermission(uint id, address user, string memory _permission) public view itemExists(id) returns (bool userPermission) {
+    //     if (keccak256(bytes(_permission)) == keccak256(bytes("view"))) {
+    //         return permissions[id][user][0];
+    //     }
+    //     else if (keccak256(bytes(_permission)) == keccak256(bytes("edit"))) {
+    //         return permissions[id][user][1];
+    //     }
+    //     else if (keccak256(bytes(_permission)) == keccak256(bytes("grant"))) {
+    //         return permissions[id][user][2];
+    //     } else require(false, "use view, edit and grant");
+    // }
+    // function getPermissions(uint id, address user) public view itemExists(id) returns (Permission memory userHasPermission) {
+    //     return streamIdToPermissions[id][user];
+    // }
+
+    function addressToString(address _address) public pure returns(string memory) {
+       bytes32 _bytes = bytes32(uint256(uint160(_address)));
+       bytes memory _hex = "0123456789abcdef";
+       bytes memory _string = new bytes(42);
+       _string[0] = "0";
+       _string[1] = "x";
+       for(uint i = 0; i < 20; i++) {
+           _string[2+i*2] = _hex[uint8(_bytes[i + 12] >> 4)];
+           _string[3+i*2] = _hex[uint8(_bytes[i + 12] & 0x0f)];
+       }
+       return string(_string);
     }
 
     // function transferViewRights(string id, address recipient, uint8 amount) public itemExists(id) {
@@ -88,24 +198,7 @@ contract StreamRegistry {
     //     emit TransferedPublishRights(id, msg.sender, recipient, amount);
     // }
 
-    // // function grantPermissions(uint id, address user, bool[] memory _permission) public itemExists(id) canGrant(id) {
-    //     permissions[id][user] = _permission;
-    // }
-    
-    // function hasPermission(uint id, address user, string memory _permission) public view itemExists(id) returns (bool userPermission) {
-    //     if (keccak256(bytes(_permission)) == keccak256(bytes("view"))) {
-    //         return permissions[id][user][0];
-    //     }
-    //     else if (keccak256(bytes(_permission)) == keccak256(bytes("edit"))) {
-    //         return permissions[id][user][1];
-    //     }
-    //     else if (keccak256(bytes(_permission)) == keccak256(bytes("grant"))) {
-    //         return permissions[id][user][2];
-    //     } else require(false, "use view, edit and grant");
-    // }
-    // function getPermissions(uint id, address user) public view itemExists(id) returns (Permission memory userHasPermission) {
-    //     return streamIdToPermissions[id][user];
-    // }
+  
 // function addressToString(address _address) public pure returns (string memory _uintAsString) {
 //       uint256 _i = uint256(uint160(address(_address)));
 //     //   uint _i = uint256(_address);
@@ -126,16 +219,4 @@ contract StreamRegistry {
 //       }
 //       return string(bstr);
 //     }
-function addressToString(address _address) public pure returns(string memory) {
-       bytes32 _bytes = bytes32(uint256(uint160(_address)));
-       bytes memory _hex = "0123456789abcdef";
-       bytes memory _string = new bytes(42);
-       _string[0] = "0";
-       _string[1] = "x";
-       for(uint i = 0; i < 20; i++) {
-           _string[2+i*2] = _hex[uint8(_bytes[i + 12] >> 4)];
-           _string[3+i*2] = _hex[uint8(_bytes[i + 12] & 0x0f)];
-       }
-       return string(_string);
-    }
 }
