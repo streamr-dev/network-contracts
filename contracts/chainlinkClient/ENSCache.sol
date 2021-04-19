7af9b234c8a87445c00a9a73aa3f33d16856cf28
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2021-04-19 18:18:16 +0200

    init enscache

---
+++ contracts/chainlinkClient/ENSCache.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.3;
+
+oonable, chainlinkclient
+contract ENScache {
+
+    syncENSName(string ensname)
+
+    oracleAnswer..
+
+    getOwner(string ensname)
+    
+}
+ 
