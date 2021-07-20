9de445ad9c37663bb028bcfe8057e0ad468dcfcb
Juuso Takalainen <juuso@streamr.network>
2021-07-20 18:24:49 +0300

    fix: get clean lint result
    also add back the subgraph.yaml for now. It's not pretty, and it would be better linting process would generate whatever yaml it needs itself, but... I guess I'll let it be for now

--- packages/smartcontracts/test/StreamRegistry.test.ts
+++ packages/smartcontracts/test/StreamRegistry.test.ts
@@ -7,6 +7,7 @@ import StreamRegistryJson from '../artifacts/contracts/StreamRegistry/StreamRegi
 // import { ENSMock } from '../typechain/StreamRegistry'
 import ForwarderJson from '../test-contracts/MinimalForwarder.json'
 import { MinimalForwarder } from '../test-contracts/MinimalForwarder'
+
 import type { StreamRegistry } from '../typechain/StreamRegistry'
 
 const ethSigUtil = require('eth-sig-util')
