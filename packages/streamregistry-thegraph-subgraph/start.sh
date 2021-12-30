d1192cf24aa1f4cf6a551e696d8dac207fa78375
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2021-12-30 10:53:17 +0100

    reworked script so that it queries graphql and verifies that the storagenode is registered, indicating that the subgraph is deployed and the graph node is synced correnctly

--- packages/streamregistry-thegraph-subgraph/start.sh
+++ packages/streamregistry-thegraph-subgraph/start.sh
@@ -1,8 +1,11 @@
-CONTAINER_ALREADY_STARTED="/firstrun/CONTAINER_ALREADY_STARTED_PLACEHOLDER"
-if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-    echo "-- First container startup, waiting 30sec, then deploying subgraph --"
-    sleep 30s; npm run doAll-inside-docker-dev
-    touch $CONTAINER_ALREADY_STARTED
+result=$(curl --silent -X POST -H "Content-Type: application/json" -d '{"query":"{nodes{id}}"}' http://streamr-dev-thegraph-node:8000/subgraphs/name/streamr-dev/network-contracts) 
+echo $result
+if ! [ "$result" = '{"data":{"nodes":[{"id":"0xde1112f631486cfc759a50196853011528bc5fa0"}]}}' ]; then
+    echo "Not deployed yet, trying to deploy subgraph"
+    npm run doAll-inside-docker-dev
+    set -e
+    curl --write-out %{http_code} --silent --output /dev/null -X POST -H "Content-Type: application/json" -d '{"query":"{nodes{id}}"}' http://streamr-dev-thegraph-node:8000/subgraphs/name/streamr-dev/network-contracts
 else
-    echo "-- Not first container startup, doing nothing.--"
+    echo "Subgraph already deployed, doing nothing"
+    exit 0
 fi
