014f052350fa3450f91ed659264008134c5fd0d7
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2022-01-21 15:19:23 +0100

    revert start script of deploy container, something seems off in CI

--- packages/streamregistry-thegraph-subgraph/start.sh
+++ packages/streamregistry-thegraph-subgraph/start.sh
@@ -1,11 +1,8 @@
-result=$(curl --silent -X POST -H "Content-Type: application/json" -d '{"query":"{nodes{id}}"}' http://streamr-dev-thegraph-node:8000/subgraphs/name/streamr-dev/network-contracts) 
-echo $result
-if ! [ "$result" = '{"data":{"nodes":[{"id":"0xde1112f631486cfc759a50196853011528bc5fa0"}]}}' ]; then
-    echo "Not deployed yet, trying to deploy subgraph"
-    npm run doAll-inside-docker-dev
-    set -e
-    curl --write-out %{http_code} --silent --output /dev/null -X POST -H "Content-Type: application/json" -d '{"query":"{nodes{id}}"}' http://streamr-dev-thegraph-node:8000/subgraphs/name/streamr-dev/network-contracts
+CONTAINER_ALREADY_STARTED="/firstrun/CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    touch $CONTAINER_ALREADY_STARTED
+    echo "-- First container startup, waiting 30sec, then deploying subgraph --"
+    sleep 30s; npm run doAll-inside-docker-dev
 else
-    echo "Subgraph already deployed, doing nothing"
-    exit 0
+    echo "-- Not first container startup, doing nothing.--"
 fi
