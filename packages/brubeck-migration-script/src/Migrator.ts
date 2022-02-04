b385c7883c4f8893fb8f423df4fae671cfb790ec
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2022-02-04 16:53:40 +0100

    further writing logic for migration

--- packages/brubeck-migration-script/src/Migrator.ts
+++ packages/brubeck-migration-script/src/Migrator.ts
@@ -1,6 +1,7 @@
+/* eslint-disable max-len */
 import { TimerOptions } from 'timers'
 
-import { Wallet } from '@ethersproject/wallet'
+import Debug from 'debug'
 import hhat from 'hardhat'
 import { BigNumber, BigNumberish } from '@ethersproject/bignumber'
 import { MaxInt256 } from '@ethersproject/constants'
@@ -33,194 +34,111 @@ export type StreamData = {
 }
 
 export class Migrator {
+    private debug = Debug('Migrator')
 
     private registryFromAdmin : StreamRegistry
+
     private registryFromMigrator : StreamRegistry
 
-    constructor() {
+    async migrate(streams:any[]): Promise<void> {
+        const streamDatas = await Migrator.convertToStreamDataArray(streams)
+        this.sendStreamsToChain(streamDatas)
+    }
+
+    static async convertToStreamDataArray(streams:any[]): Promise<StreamData[]> {
+        const streamDatas: StreamData[] = []
+        streams.forEach((stream:any) => {
+            Object.keys(stream).forEach((user:string) => {
+                streamDatas.push({
+                    id: stream.id,
+                    metadata: stream.metadata,
+                    user,
+                    permissions: stream[user].map(Migrator.convertPermissions)
+                })
+            })
+        })
+        return streamDatas
+    }
+
+    async connectToStreamRegistry() {
         const networkProvider = new ethers.providers.JsonRpcProvider(CHAIN_NODE_URL)
         const adminWallet = new ethers.Wallet(ADMIN_PRIVATEKEY, networkProvider)
         const migratorWallet = new ethers.Wallet(MIGRATOR_PRIVATEKEY, networkProvider)
-
         const streamregistryFactory = await ethers.getContractFactory('StreamRegistry')
         const registry = await streamregistryFactory.attach(STREAMREGISTRY_ADDRESS)
         const registryContract = await registry.deployed()
         this.registryFromAdmin = await registryContract.connect(adminWallet) as StreamRegistry
         this.registryFromMigrator = await registryContract.connect(migratorWallet) as StreamRegistry
-        const mtx = await this.registryFromAdmin.grantRole(await this.registryFromAdmin.TRUSTED_ROLE(), migratorWallet.address)
+        const mtx = await this.registryFromAdmin.grantRole(await this.registryFromAdmin.TRUSTED_ROLE(),
+            migratorWallet.address)
         await mtx.wait()
-        console.log('added migrator role to ' + migratorWallet.address)
+        this.debug.log('added migrator role to ' + migratorWallet.address)
     }
-}
 
-
-
-// one transaction with 30 streams, one permission each costs about 2mio gas
-// polygon has 20 mio blockgaslimit, 5 transactions should fit in one block (depending on how many
-// permissions each stream has)
-const convertPermissions = (permissions: string[]) => {
-    const permissionSet = {
-        edit: false,
-        canDelete: false,
-        publishExpiration: BigNumber.from(0),
-        subscribeExpiration: BigNumber.from(0),
-        share: false,
-    }
-    permissions.forEach((el) => {
-        switch (el) {
-            case 'stream_edit':
-                permissionSet.edit = true
-                break
-            case 'stream_delete':
-                permissionSet.canDelete = true
-                break
-            case 'stream_publish':
-                permissionSet.publishExpiration = MaxInt256
-                break
-            case 'stream_subscribe':
-                permissionSet.subscribeExpiration = MaxInt256
-                break
-            case 'stream_share':
-                permissionSet.share = true
-                break
-            default:
-                break
-        }
-    })
-    return permissionSet
-}
-
-const sendStreamsToChain = async (streams: StreamData[], nonceParam: number) => {
-    // const permissions = new Array(streams.length)
-    // permissions.fill([{
-    //     edit: true,
-    //     canDelete: true,
-    //     publishExpiration: 0,
-    //     subscribeExpiration: 0,
-    //     share: true,
-    // }])
-    // const fakeAddr = Wallet.createRandom().address
-    // const users = new Array(streams.length)
-    // users.fill(fakeAddr)
-    const metadatas = new Array(streams.length)
-    metadatas.fill('')
-
-    // nonceManager.setTransactionCount(nonce)
-    // nonce += 1
-
-    try {
-        const tx = await registryFromMigrator.populateTransaction.trustedBulkAddStreams(
-            streams.map((el) => el.id),
-            streams.map((el) => el.user),
-            metadatas,
-            streams.map((el) => el.permissions)
-        )
-        tx.nonce = nonceParam
-        tx.gasPrice = BigNumber.from(1)
-        // tx.gasLimit = BigNumber.from(6000000)
-        // const signedtx = await migratorWallet.signTransaction(tx)
-        const tx2 = await migratorWallet.sendTransaction(tx)
-        // eslint-disable-next-line no-underscore-dangle
-        console.log(`sent out tx: nonce: ${tx2.nonce}, gas: ${parseInt(tx2.gasLimit._hex, 16)}, gasPrice: ${tx2.gasPrice?.toNumber()}`)
-        const timer = setTimeout(async () => {
-            console.log(`nothing happening for 20s, resending tx with nonce ${tx2.nonce}`)
-            const newGasPrice = (tx2.gasPrice as BigNumber).toNumber() + 150 //* 1.2
-            // const newGasPrice = 200
-            if (tx2.gasPrice) { tx.gasPrice = BigNumber.from(Math.ceil(newGasPrice)) }
-            const txResend = await migratorWallet.sendTransaction(tx)
-            console.log(`resent tx with nonce: ${txResend.nonce}, gas: ${parseInt(txResend.gasLimit._hex, 16)}, gasPrice: ${txResend.gasPrice?.toNumber()}`)
-            await txResend.wait()
-            console.log('mined resent tx with nonce ' + txResend.nonce)
-        }, 30000)
-        // console.log(`tx2: ${JSON.stringify(tx2)}`)
-        await tx2.wait()
-        clearTimeout(timer)
-        console.log('mined tx with nonce ' + tx2.nonce)
-    } catch (err: any) {
-        if (err.code === 'TRANSACTION_REPLACED') { console.log('a transaction got replaced') } else { console.log(err) }
-    }
-}
-
-const addAndSendStreamPermission = async (streamID: string, user:string, permissionStrings:string[], lineNr: number) => {
-    if (lineNr <= sucessfulLineNumber) {
-        return Promise.resolve()
-    }
-    sucessfulLineNumber = lineNr
-    process.stdout.write('.')
-    const permissions = convertPermissions(permissionStrings)
-    streamsToMigrate.push({
-        id: streamID, user, permissions
-    })
-    if (streamsToMigrate.length >= 60) {
-        const clonedArr = streamsToMigrate.map((a) => ({
-            ...a
-        }))
-        // const a1 = streamsToMigrate.splice(0, 50)
-        // const a2 = streamsToMigrate.splice(0, 50)
-        streamsToMigrate = []
-        transactionData.push({
-            streamdata: clonedArr,
-            nonce
-        })
-        nonce += 1
-        if (transactionData.length >= 5) {
-            const promises = transactionData.map(
-                (data) => sendStreamsToChain(data.streamdata, data.nonce)
+    async sendStreamsToChain(streams: StreamData[]) {
+        const metadatas = new Array(streams.length)
+        metadatas.fill('')
+        try {
+            // const tx = await this.registryFromMigrator.populateTransaction.trustedBulkAddStreams(
+            const tx = await this.registryFromMigrator.trustedBulkAddStreams(
+                streams.map((el) => el.id),
+                streams.map((el) => el.user),
+                metadatas,
+                streams.map((el) => el.permissions)
             )
-            await Promise.all(promises)
-            // eslint-disable-next-line require-atomic-updates
-            transactionData = []
-            // fs.writeFile(PROGRESS_FILENAME, sucessfulLineNumber.toString(), (err) => {
-            //     if (err) { throw err }
-            //     console.log('saved current linenumber ' + sucessfulLineNumber)
-            // })
-            fs.writeFileSync(PROGRESS_FILENAME, sucessfulLineNumber.toString())
-            console.log('saved current linenumber ' + sucessfulLineNumber)
-        }
-        // nonce += 1
-        // nonceManager.setTransactionCount(nonce)
-        // nonce += 1
-        // nonceManager.setTransactionCount(nonce)
-        // sendStreamsToChain(a2)
-        // await new Promise((resolve) => setTimeout(resolve, 500000))
-    }
-    return Promise.resolve()
-}
-
-async function main() {
-    
-    let resolver: any
-    // const promise = new Promise((resolve) => { resolver = resolve })
-
-            const words = line.split('\t')
-            const streamid = words[0]
-            const user = words[1]
-            const permissions = JSON.parse(words[2]) as string[]
-            // const address = id.split('/')[0]
-            // if (ethers.utils.isAddress(address)) {
-            if (ethers.utils.isAddress(user)) {
-                // valids += 1
-                // if (!id.includes('metrics')) { withoutMetrics += 1 }
-                // console.log(id)
-                await addAndSendStreamPermission(streamid, user, permissions, lineNr)
+            // eslint-disable-next-line no-underscore-dangle
+            // const timer = setTimeout(async () => {
+            //     console.log(`nothing happening for 20s, resending tx with nonce ${tx2.nonce}`)
+            //     const newGasPrice = (tx2.gasPrice as BigNumber).toNumber() //* 1.2
+            //     // const newGasPrice = 200
+            //     if (tx2.gasPrice) { tx.gasPrice = BigNumber.from(Math.ceil(newGasPrice)) }
+            //     const txResend = await migratorWallet.sendTransaction(tx)
+            //     console.log(`resent tx with nonce: ${txResend.nonce}, gas: ${parseInt(txResend.gasLimit._hex, 16)}, gasPrice: ${txResend.gasPrice?.toNumber()}`)
+            //     await txResend.wait()
+            //     console.log('mined resent tx with nonce ' + txResend.nonce)
+            // }, 30000)
+            // console.log(`tx2: ${JSON.stringify(tx2)}`)
+            await tx.wait()
+            // clearTimeout(timer)
+            this.debug.log('mined tx with nonce ' + tx.nonce)
+        } catch (err: any) {
+            if (err.code === 'TRANSACTION_REPLACED') {
+                this.debug.log('a transaction got replaced')
+            } else {
+                this.debug.log(err)
             }
-            // }
-            s.resume()
+        }
+    }
+
+    static convertPermissions(permissions: string[]) {
+        const permissionSet = {
+            edit: false,
+            canDelete: false,
+            publishExpiration: BigNumber.from(0),
+            subscribeExpiration: BigNumber.from(0),
+            share: false,
+        }
+        permissions.forEach((el) => {
+            switch (el) {
+                case 'stream_edit':
+                    permissionSet.edit = true
+                    break
+                case 'stream_delete':
+                    permissionSet.canDelete = true
+                    break
+                case 'stream_publish':
+                    permissionSet.publishExpiration = MaxInt256
+                    break
+                case 'stream_subscribe':
+                    permissionSet.subscribeExpiration = MaxInt256
+                    break
+                case 'stream_share':
+                    permissionSet.share = true
+                    break
+                default:
+                    break
+            }
         })
-            .on('error', (err: any) => {
-                console.log('Error while reading file.', err)
-            })
-            .on('end', () => {
-                console.log(`Read ${lineNr} lines, ${valids} valid ids, ${withoutMetrics} without metrics.`)
-                resolver(true)
-            }))
-    return promise
+        return permissionSet
+    }
 }
-
-main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
-
