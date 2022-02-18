af87bed0a063e00b6ebe92e3b688944db06b4b9f
Kare Nuorteva <kare.nuorteva@me.com>
2022-02-18 18:03:47 +0200

    feat(config): load configuration based on NODE_ENV environment variable

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -26,3 +26,15 @@ export const loadConfig = (env: Environment): Chains => {
     const chain: Chains = networks[env]
     return chain
 }
+
+export const loadConfigFromNodeEnv = (): Chains => {
+    const nodeEnv = process.env.NODE_ENV
+    if (nodeEnv === undefined) {
+        throw new Error("NODE_ENV environment variable is not set")
+    }
+    if (nodeEnv !== "production" && nodeEnv !== "development") {
+        throw new Error("NODE_ENV environment variable value must be either 'production' or 'development'")
+    }
+    const env: Environment = nodeEnv
+    return loadConfig(env)
+}
--- packages/config/test/index.test.ts
+++ packages/config/test/index.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from "mocha"
 import { assert } from "chai"
-import { Chains, loadConfig } from "../src/index"
+import { Chains, loadConfig, loadConfigFromNodeEnv } from "../src/index"
 
 describe("Load configuration from JSON file", () => {
     it("ethereum chain id is 1", () => {
@@ -26,5 +26,35 @@ describe("Load configuration from JSON file", () => {
         const rpcHttpUrl = config.polygon.rpcHttpUrl
         const expected = "https://polygon-rpc.com"
         assert.equal(rpcHttpUrl, expected, `Expecting prod polygon RPC URL to equal ${expected}, got '${rpcHttpUrl}'`)
+    })
+})
+describe("Load configuration based on NODE_ENV environment variable", () => {
+    it("ethereum chain id is 1", () => {
+        process.env.NODE_ENV = "production"
+        const config: Chains = loadConfigFromNodeEnv()
+        const chainId: number = config.ethereum.id
+        const expected = 1
+        assert.equal(chainId, expected, `Expecting ethereum prod chain id to equal ${expected}, got '${chainId}'`)
+    })
+    it("development chain id is 8995", () => {
+        process.env.NODE_ENV = "development"
+        const config: Chains = loadConfigFromNodeEnv()
+        const chainId: number = config.ethereum.id
+        const expected = 8995
+        assert.equal(chainId, expected, `Expecting ethereum dev chain id to equal ${expected}, got '${chainId}'`)
+    })
+    it("errors when NODE_ENV is not set", () => {
+        delete process.env.NODE_ENV
+        assert.throws(() => {
+            /* eslint-disable @typescript-eslint/no-unused-vars */
+            const config: Chains = loadConfigFromNodeEnv()
+        }, /NODE_ENV environment variable is not set/)
+    })
+    it("errors when NODE_ENV is something else than 'production' or 'development'", () => {
+        process.env.NODE_ENV = "dev"
+        assert.throws(() => {
+            /* eslint-disable @typescript-eslint/no-unused-vars */
+            const config: Chains = loadConfigFromNodeEnv()
+        }, /NODE_ENV environment variable value must be either 'production' or 'development'/)
     })
 })
