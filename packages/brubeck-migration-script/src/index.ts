a84c5f106923e8961b621ac7315fc54597b40cfd
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2022-03-10 14:00:57 +0100

    sql workshop with teo and eric: added correct selection on public permission with paying attention to corrupt data, added ends_at field to results

--- packages/brubeck-migration-script/src/index.ts
+++ packages/brubeck-migration-script/src/index.ts
@@ -6,6 +6,14 @@
 // select DISTINCT stream.id, user.username, permission.operation from user, stream, permission where stream.migrate_to_brubeck = 1
 // and user.id = permission.user_id and permission.stream_id = stream.id and permission.operation != 'stream_get' order by stream.id, user.username limit 10;
 
+// select DISTINCT stream.id, stream.description, stream.partitions, stream.inactivity_threshold_hours, user.username, permission.operation 
+// from user, stream, permission 
+// where stream.migrate_to_brubeck = 1
+// and (user.id = permission.user_id OR permission.anonymous is true)
+// and permission.stream_id = stream.id
+// and permission.operation != 'stream_get' 
+// order by stream.id, user.username;
+
 // select DISTINCT stream.id, stream.description, stream.partitions, stream.inactivity_threshold_hours, user.username, permission.operation, stream_storage_node.storage_node_address 
 // from user, stream, permission, stream_storage_node 
 // where stream.migrate_to_brubeck = 1
@@ -14,6 +22,115 @@
 // and permission.stream_id = stream.id 
 // and permission.operation != 'stream_get' 
 // order by stream.id, user.username;
+
+// ***
+// // only not anonymous
+// select DISTINCT stream.id, stream.description, stream.partitions, stream.inactivity_threshold_hours, user.username, permission.operation, permission.anonymous, stream_storage_node.storage_node_address
+// from stream
+// inner join permission on stream.id = permission.stream_id
+// inner join user on permission.user_id = user.id
+// left outer join stream_storage_node on stream.id = stream_storage_node.stream_id
+// where stream.migrate_to_brubeck = 1
+// and permission.operation != 'stream_get' 
+// order by stream.id, user.username;
+
+// // only anonymouse
+// select DISTINCT stream.id, stream.description, stream.partitions, stream.inactivity_threshold_hours, permission.operation, permission.anonymous, stream_storage_node.storage_node_address
+// from stream
+// inner join permission on stream.id = permission.stream_id
+// left outer join stream_storage_node on stream.id = stream_storage_node.stream_id
+// where stream.migrate_to_brubeck = 1
+// and permission.anonymous is true
+// and permission.operation != 'stream_get' 
+// order by stream.id;
+
+
+// select DISTINCT stream.id, stream.description, stream.partitions, stream.inactivity_threshold_hours, user.username, permission.operation, permission.anonymous, stream_storage_node.storage_node_address
+// from stream, permission, user
+// inner join permission on stream.id = permission.stream_id
+// -- inner join user on permission.user_id = user.id
+// left outer join stream_storage_node on stream.id = stream_storage_node.stream_id
+// where stream.migrate_to_brubeck = 1
+// and (user.id = permission.user_id OR permission.anonymous is true)
+// and permission.operation != 'stream_get' 
+// order by stream.id, user.username;
+
+
+// select * from (
+//     select DISTINCT stream.id, stream.description, stream.partitions, stream.inactivity_threshold_hours, 
+//     case when permission.anonymous is false then (select username from user where user.id = permission.user_id) else "asdf" end as username, permission.operation, permission.anonymous, stream_storage_node.storage_node_address
+//     from stream
+//     -- inner join permission on stream.id = permission.stream_id
+//     -- join user on permission.user_id = user.id
+//     left outer join stream_storage_node on stream.id = stream_storage_node.stream_id
+//     where stream.migrate_to_brubeck = 1
+//     and permission.operation != 'stream_get'
+//     order by stream.id, username
+//     ) as t
+//     where username = 'asdf'
+
+
+
+//     select DISTINCT stream.id, stream.description, stream.partitions, stream.inactivity_threshold_hours, user.username, permission.operation, permission.anonymous, stream_storage_node.storage_node_address
+// from stream, permission, user, stream_storage_node
+// -- left outer join stream_storage_node on 
+// where stream.migrate_to_brubeck = 1
+// and stream.id = permission.stream_id
+// and (user.id = permission.user_id OR permission.anonymous is true)
+// and stream_storage_node.stream_id = stream.id
+// and permission.operation != 'stream_get' 
+// order by stream.id, user.username;
+
+
+
+
+
+// TEMP WORKING SOLUTION
+
+// (select DISTINCT stream.id, stream.description, stream.partitions, stream.inactivity_threshold_hours, user.username, permission.operation, permission.anonymous, stream_storage_node.storage_node_address
+//     from stream
+//     inner join permission on stream.id = permission.stream_id
+//     inner join user on permission.user_id = user.id
+//     left outer join stream_storage_node on stream.id = stream_storage_node.stream_id
+//     where stream.migrate_to_brubeck = 1
+//     and permission.operation != 'stream_get' 
+//     order by stream.id, user.username)
+    
+//     union
+    
+//     (select DISTINCT stream.id, stream.description, stream.partitions, stream.inactivity_threshold_hours, null, permission.operation, permission.anonymous, stream_storage_node.storage_node_address
+//     from stream
+//     inner join permission on stream.id = permission.stream_id
+//     left outer join stream_storage_node on stream.id = stream_storage_node.stream_id
+//     where stream.migrate_to_brubeck = 1
+//     and permission.anonymous is true
+//     and permission.operation != 'stream_get' 
+//     order by stream.id)
+
+
+// BETTER
+// select * from (
+//     select DISTINCT stream.id,
+//         stream.description,
+//         stream.partitions,
+//         stream.inactivity_threshold_hours,
+//         case 
+//             when permission.anonymous is false then (select username from user where user.id = permission.user_id) 
+//             else "anonymous"
+//         end as username, 
+//         permission.operation,
+//         permission.ends_at,
+//         stream_storage_node.storage_node_address
+//     from stream
+//     left join permission on permission.stream_id = stream.id
+//     left outer join stream_storage_node on stream.id = stream_storage_node.stream_id
+//     where stream.migrate_to_brubeck = 1
+//     and permission.operation != 'stream_get'
+//     and (permission.ends_at is null OR
+//         permission.ends_at > now())
+//     order by stream.id, username
+// ) as t
+// where username is not null
 
 // debug update migration flag to 1
 // UPDATE stream SET migrate_to_brubeck = 1 LIMIT 100;
