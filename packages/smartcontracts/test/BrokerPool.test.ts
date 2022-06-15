f45251d849edd5eb1bf1b236e7111f60c18df679
jtakalai <juuso@streamr.network>
2022-06-15 15:00:14 +0300

    ETH-290: BrokerPool contract (#317)
    * fix: lint
    
    * feat: BrokerPool contract and a basic happy path test
    
    * cleanup: remove console.log
    
    * fix: remove function stub
    
    add later if needed
    
    * refactor: commented out unallocatedWei
    
    tracking it seems pointless at this point; after all, it's supposed to at all times be same as the current token balance
    
    * refactor: allow additional staking

---
+++ packages/smartcontracts/test/BrokerPool.test.ts
@@ -0,0 +1,73 @@
+import { waffle } from "hardhat"
+import { expect, use } from "chai"
+import { utils } from "ethers"
+
+import {
+    deployTestContracts,
+    deployBountyContract,
+    TestContracts,
+    deployBrokerPool,
+    advanceToTimestamp,
+    getBlockTimestamp
+} from "./utils"
+
+const { parseEther, formatEther } = utils
+
+use(waffle.solidity)
+
+describe("BrokerPool", (): void => {
+    const [
+        admin,
+        broker,
+        investor,
+        sponsor,
+    ] = waffle.provider.getWallets()
+
+    let contracts: TestContracts
+
+    before(async (): Promise<void> => {
+        contracts = await deployTestContracts(admin)
+
+        const { token } = contracts
+        await (await token.mint(sponsor.address, parseEther("1000000"))).wait()
+        await (await token.mint(investor.address, parseEther("1000000"))).wait()
+    })
+
+    it("allows invest and withdraw", async function(): Promise<void> {
+        const { token } = contracts
+        const pool = await deployBrokerPool(broker, token)
+        await (await token.connect(investor).approve(pool.address, parseEther("1000"))).wait()
+        await expect(pool.connect(investor).invest(parseEther("1000")))
+            .to.emit(pool, "InvestmentReceived").withArgs(investor.address, parseEther("1000"))
+        const freeFundsAfterInvest = await token.balanceOf(pool.address) // await pool.unallocatedWei()
+
+        await expect(pool.connect(investor).withdraw(parseEther("1000")))
+            .to.emit(pool, "InvestmentReturned").withArgs(investor.address, parseEther("1000"))
+        const freeFundsAfterWithdraw = await token.balanceOf(pool.address) // await pool.unallocatedWei()
+
+        expect(formatEther(freeFundsAfterInvest)).to.equal("1000.0")
+        expect(formatEther(freeFundsAfterWithdraw)).to.equal("0.0")
+    })
+
+    it("stakes, and unstakes with gains", async function(): Promise<void> {
+        const { token } = contracts
+        const bounty = await deployBountyContract(contracts)
+        const pool = await deployBrokerPool(broker, token)
+        await (await token.connect(investor).transferAndCall(pool.address, parseEther("1000"), "0x")).wait()
+        await (await token.connect(sponsor).transferAndCall(bounty.address, parseEther("1000"), "0x")).wait()
+
+        const balanceBefore = await token.balanceOf(pool.address)
+        const timeAtStart = await getBlockTimestamp()
+
+        await advanceToTimestamp(timeAtStart, "Stake to bounty")
+        await expect(pool.stake(bounty.address, parseEther("1000")))
+            .to.emit(pool, "Staked").withArgs(bounty.address, parseEther("1000"))
+
+        await advanceToTimestamp(timeAtStart + 1000, "Unstake from bounty")
+        await expect(pool.unstake(bounty.address))
+            .to.emit(pool, "Unstaked").withArgs(bounty.address, parseEther("1000"), parseEther("1000"))
+
+        const gains = (await token.balanceOf(pool.address)).sub(balanceBefore)
+        expect(formatEther(gains)).to.equal("1000.0")
+    })
+})
