e9306c9fb32a1465d8427d8cde52f9fc3923d7cb
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2022-08-12 16:37:46 +0200

    fix path to mainchain from inside docker in hardhat config

--- packages/docker-dev-chain-init/hardhat.config.js
+++ packages/docker-dev-chain-init/hardhat.config.js
@@ -46,7 +46,7 @@ module.exports = {
         },
         localmainchain: {
             chainId: 8995,
-            url: "http://localhost:8545",
+            url: "http://10.200.10.1:8545",
             accounts: ['0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0']
         },
         polygonTestMumbai1: {
