de63429872a10743c1979f7f42f8552d9935ff8c
Juuso Takalainen <juuso@streamr.network>
2022-10-07 14:38:32 +0300

    fix: patch GSN

--- packages/smartcontracts/scripts/transactThroughGSN.ts
+++ packages/smartcontracts/scripts/transactThroughGSN.ts
@@ -43,15 +43,23 @@ async function main() {
     const gsnWeb3Provider = RelayProvider.newProvider({ provider: web3provider, config })
     await gsnWeb3Provider.init()
 
+    // patch the GSN class so that it works nicely with ethers.js (it's made for web3.js, BOOO!)
+    const gsnContractInteractor = gsnWeb3Provider.relayClient.dependencies.contractInteractor
+    const gsnOriginalGetter = gsnContractInteractor.getMaxViewableGasLimit
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore We're deliberately patching it to return a string, while the type says BN
+    gsnContractInteractor.getMaxViewableGasLimit = (...args) => gsnOriginalGetter.apply(gsnContractInteractor, args).then((x) => x.toString())
+
     const gsnProvider = new ethers.providers.Web3Provider(gsnWeb3Provider)
 
     // empty account, no eth, no gas
     const account = new ethers.Wallet('0x52090fc090bd60b26f8e907287d1b5a02105d5b59d1f6840f32a4cbadf81810d', gsnProvider)
     gsnWeb3Provider.addAccount(account.privateKey)
 
+    // getSigner produces a JsonRpcSigner which apparently works differently from Wallet
     const registryThroughGSN = registry.connect(gsnProvider.getSigner(account.address))
-    // await registryThroughGSN.createStream("/samgsntest1", "metadata")
-    await registryThroughGSN.deleteStream("0x9cdfd342181b6b6bfc350f3f96a44aad499d0536/samgsntest1")
+    await registryThroughGSN.createStream("/samgsntest1", "metadata")
+    // await registryThroughGSN.deleteStream("0x9cdfd342181b6b6bfc350f3f96a44aad499d0536/samgsntest1")
 
     // const storageRegistryThroughGSN = storageRegistry.connect(etherProvider.getSigner(from)) as StreamStorageRegistryV2
     // await storageRegistryThroughGSN.addStorageNode(
