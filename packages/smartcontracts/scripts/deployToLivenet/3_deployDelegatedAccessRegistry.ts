3984ec68e668af568beca77ba9fab34c9feec3b3
bearni95 <bernatcanal@gmail.com>
2022-10-12 10:56:04 +0200

    added method getDelegatedWalletFor on DelegatedAccessRegistry smart contract

--- packages/smartcontracts/scripts/deployToLivenet/3_deployDelegatedAccessRegistry.ts
+++ packages/smartcontracts/scripts/deployToLivenet/3_deployDelegatedAccessRegistry.ts
@@ -1,18 +1,40 @@
 import { JsonRpcProvider } from '@ethersproject/providers'
-import { Wallet } from 'ethers'
+import { BigNumber, Wallet } from 'ethers'
 import hhat from 'hardhat'
 const { ethers } = hhat
+import axios from 'axios'
 
 // localsidechain
 const chainURL = 'http://10.200.10.1:8546'
 const privKeyStreamRegistry = ''
+
+async function getGasStationPrices(): Promise<{maxFeePerGas: BigNumber, maxPriorityFeePerGas: BigNumber}> {
+    const { data } = await axios({
+        method: 'get',
+        url: 'https://gasstation-mainnet.matic.network/v2'
+    })
+    const maxFeePerGas = ethers.utils.parseUnits(
+        Math.ceil(data.fast.maxFee) + '',
+        'gwei'
+    )
+    const maxPriorityFeePerGas = ethers.utils.parseUnits(
+        Math.ceil(data.fast.maxPriorityFee) + '',
+        'gwei'
+    )
+
+    return { maxFeePerGas, maxPriorityFeePerGas }
+}
 
 async function main() {
     const wallet = new Wallet(privKeyStreamRegistry, new JsonRpcProvider(chainURL))
 
     const DelegatedAccessRegistry = await ethers.getContractFactory('DelegatedAccessRegistry', wallet)
 
-    const tx = await DelegatedAccessRegistry.deploy()
+    const { maxFeePerGas, maxPriorityFeePerGas } = await getGasStationPrices()
+
+    const tx = await DelegatedAccessRegistry.deploy({
+        maxFeePerGas, maxPriorityFeePerGas
+    })
 
     const instance = await tx.deployed()
 
--- packages/smartcontracts/test/GatedChatRooms/DelegatedAccessRegistry.test.ts
+++ packages/smartcontracts/test/GatedChatRooms/DelegatedAccessRegistry.test.ts
@@ -103,6 +103,14 @@ describe('DelegatedAccessRegistry', (): void => {
         expect(mainWallet).to.equal(wallets[0].address)
     })
 
+    it ('happy-path for `getDelegatedWalletFor`', async () => {
+        const delegatedWallet = await contract.getDelegatedWalletFor(
+            wallets[0].address
+        )
+        
+        expect(delegatedWallet).to.equal(delegated.address)
+    })
+
     it ('happy-path for `isMainWallet`', async () => {
         const isMainWallet = await contract.isMainWallet(
             wallets[0].address
