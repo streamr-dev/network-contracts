6c5d01b737e42ac490b38c4c96d99edef24959d8
Juuso Takalainen <juuso@streamr.network>
2022-11-23 14:50:54 +0200

    refactor: whitespace fixes

--- packages/network-contracts/contracts/GatedChatRooms/UpgradeableJoinPolicyFactory.sol
+++ packages/network-contracts/contracts/GatedChatRooms/UpgradeableJoinPolicyFactory.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.9;
 
-import "../StreamRegistry/StreamRegistryV3.sol"; 
+import "../StreamRegistry/StreamRegistryV3.sol";
 import "./ERC20JoinPolicy.sol";
 import "./ERC721JoinPolicy.sol";
 import "./ERC1155JoinPolicy.sol";
@@ -25,9 +25,9 @@ contract JoinPolicyFactory is Initializable {
     mapping(bytes32 => address) public registeredPolicies;
 
     event Registered(
-        address indexed tokenAddress, 
-        string indexed streamId, 
-        address policyAddress, 
+        address indexed tokenAddress,
+        string indexed streamId,
+        address policyAddress,
         bytes32 policyId
     );
 
@@ -125,6 +125,4 @@ contract JoinPolicyFactory is Initializable {
             policyId
         );
     }
-
-    
 }
