897351caad1196168b2f994fe8fa6c9db346d9d2
jtakalai <juuso@streamr.network>
2023-01-18 12:49:34 +0200

    doc: add comment

--- packages/hub-contracts/test/contracts/MarketplaceV3.test.ts
+++ packages/hub-contracts/test/contracts/MarketplaceV3.test.ts
@@ -54,7 +54,7 @@ describe("MarketplaceV3", () => {
             const market = await deployMarketplace()
 
             // upgrade the marketplace to the new version
-            const marketNewFactory = await getContractFactory("MarketplaceV3") // TODO: replace with the new marketplace contract
+            const marketNewFactory = await getContractFactory("MarketplaceV3") // TODO: replace with the new marketplace contract if V3 gets an update, e.g. V3_1
             const marketNew = await upgrades.upgradeProxy(market, marketNewFactory)
 
             expect(market.address)
