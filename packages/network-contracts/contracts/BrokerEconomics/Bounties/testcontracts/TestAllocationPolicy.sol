4374b95b30a5e842ca4dfbcca9ea32486089768a
Juuso Takalainen <juuso@streamr.network>
2023-02-10 14:25:23 +0200

    test: same ordering as in the file
    TODO: fix the numbering

--- packages/network-contracts/contracts/BrokerEconomics/Bounties/testcontracts/TestAllocationPolicy.sol
+++ packages/network-contracts/contracts/BrokerEconomics/Bounties/testcontracts/TestAllocationPolicy.sol
@@ -27,14 +27,14 @@ contract TestAllocationPolicy is IAllocationPolicy, Bounty {
         // console.log("setParam", testCase);
         if (testCase == 1) {
             require(false, "test-error: setting param allocation policy");
-        } else if (testCase == 2) {
-            localData().failOnjoin = true;
-        } else if (testCase == 3) {
-            localData().failOnLeave = true;
         } else if (testCase == 4) {
             require(false);
+        } else if (testCase == 2) {
+            localData().failOnjoin = true;
         } else if (testCase == 5) {
             localData().failEmptyOnjoin = true;
+        } else if (testCase == 3) {
+            localData().failOnLeave = true;
         } else if (testCase == 6) {
             localData().failEmptyOnLeave = true;
         } else if (testCase == 7) {
