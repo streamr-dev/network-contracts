6250bd239537a9a0ac69c26b27391d01d1b2d33f
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2023-03-07 16:09:06 +0400

    reverten folder refactor

--- packages/network-contracts/contracts/BrokerEconomics/Delegation/policies/IPoolJoinPolicy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.13;
-
-interface IPoolJoinPolicy {
-    function setParam(uint256 initialMargin, uint256 minimumMarginPercent) external;
-    function canJoin(address delegator) external view returns (uint canJoin);
-}
