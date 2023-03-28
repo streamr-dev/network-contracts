0f7cc5f5a593e2c9ace2daed70dc7e24ea6401c5
Sami Gabor <samoila.gabor@yahoo.com>
2023-03-28 12:05:25 +0300

    update readme, remove graph build step when building the image

--- packages/network-subgraphs/start.sh
+++ packages/network-subgraphs/start.sh
@@ -3,7 +3,6 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup, waiting 30sec, then deploying subgraph --"
     sleep 30s;
-    npm run build
     npm run create-docker-dev
     npm run deploy-docker-dev
 else
