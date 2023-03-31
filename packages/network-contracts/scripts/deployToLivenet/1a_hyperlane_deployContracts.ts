86d38ee6503dd1886d99efb0c9f07108d3fe1db5
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2023-03-31 09:59:17 +0400

    deployscript to mumbai testnet to test with hyperlane

--- packages/network-contracts/scripts/deployToLivenet/1a_hyperlane_deployContracts.ts
+++ packages/network-contracts/scripts/deployToLivenet/1a_hyperlane_deployContracts.ts
@@ -6,8 +6,15 @@ const log = debug("Streamr:eth-init")
 
 const { ethers, upgrades } = hhat
 
-const chainURL = "http://10.200.10.1:8546"
-const privKeyStreamRegistry = "0x4059de411f15511a85ce332e7a428f36492ab4e87c7830099dadbf130f1896ae"
+// const chainURL = "http://10.200.10.1:8546"
+// const privKeyStreamRegistry = "0x4059de411f15511a85ce332e7a428f36492ab4e87c7830099dadbf130f1896ae"
+
+// mumbai
+// const chainURL = 'https://matic-mumbai.chainstacklabs.com/'
+const chainURL = 'https://rpc-mumbai.maticvigil.com'
+const privKeyStreamRegistry = "0x27e03eb2855d67f8cab10c97a2cf83c68f17d563efedd5df345b1c815c9ca936"
+// const LINKTOKEN_ADDRESS = '0x326C977E6efc84E512bB9C30f76E30c160eD06FB' // mumbai
+// const privKeyStreamRegistry = process.env.OCR_ADMIN_PRIVATEKEY || '' // also set DEBUG="*"
 
 let nodeRegistryAddress = ""
 let streamRegistryAddress = ""
@@ -25,7 +32,7 @@ async function deployStreamRegistry() {
 
 
     log("deploying Streamregistry")
-    const streamRegistryFactory = await ethers.getContractFactory("StreamRegistryV3", wallet)
+    const streamRegistryFactory = await ethers.getContractFactory("StreamRegistryV4", wallet)
     // const streamRegistryFactoryTx = await streamRegistryFactory.deploy(ensCache.address, constants.AddressZero)
     const streamRegistryFactoryTx = await upgrades.deployProxy(streamRegistryFactory,
         [Wallet.createRandom().address, Wallet.createRandom().address], { kind: "uups" })
@@ -42,7 +49,7 @@ async function deployStreamRegistry() {
     log("deploying Hyperlane ENSCache")
     const ensCacheFactory2 = await ethers.getContractFactory("ENSCacheHyV1", wallet)
     const ensCacheFactoryTx2 = await upgrades.deployProxy(ensCacheFactory2,
-        [streamRegistry.address, streamRegistry.address, streamRegistry.address], { kind: "uups" })
+        [streamRegistry.address, "0xF782C6C4A02f2c71BB8a1Db0166FAB40ea956818", "0xF90cB82a76492614D07B82a7658917f3aC811Ac1"], { kind: "uups" })
     const ensCache2 = await ensCacheFactoryTx2.deployed()
     log(`Hyperlane ENSCache deployed at ${ensCache2.address}`)
     log(`setting Hyperlane ENSCache address in Streamregistry`)
@@ -62,8 +69,6 @@ async function deployStreamRegistry() {
 async function main() {
     wallet = new Wallet(privKeyStreamRegistry, new JsonRpcProvider(chainURL))
     log(`wallet address ${wallet.address}`)
-    const initialNodes: string[] = []
-    const initialMetadata: string[] = []
 
     await deployStreamRegistry()
 }
