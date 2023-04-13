2b8e553f6be142b857620fda7659ee9db3bc0e77
Sam <christoph-samuel.pitter@streamr.network>
2023-04-13 14:00:13 +0200

    Eth 489 custom ens script (#521)
    * added new package for ens sync script that will replace chainlink ens workflow
    
    * catching events is working
    
    * feat: add deployEns script
    
    * wip: script catches ENS query event
    
    * wip: creates stream with random domain and subdomain
    
    * fix: lint errors
    
    * feat: new ENSCache is querying old one firest
    
    * ens sync script is more fault tolerant, tested interaction with enscacheV1 and chache invalidation
    
    * script can handle multiple requests in one block now, and can handle multiple instances running, with a delay, to enable some fault tolerance
    
    * more concurrency handling, of multiple scripts running simultaneously and multiple requests coming in simultaneously
    
    * make new ENSCache upgradable
    
    * address update
    
    * feat: simplify script a lot; clean up env variables; ENSCacheV1 not requriered any more
    
    ---------
    
    Co-authored-by: Sami Gabor <samoila.gabor@yahoo.com>

---
+++ packages/ens-sync-script/src/index.ts
@@ -0,0 +1,130 @@
+/* eslint-disable no-console */
+import { Contract } from "@ethersproject/contracts"
+import { InfuraProvider, JsonRpcProvider, Provider } from "@ethersproject/providers"
+import { Wallet } from "@ethersproject/wallet"
+import { Chains } from "@streamr/config"
+import { createRequire } from "module"
+const require = createRequire(import.meta.url)
+const ABIenscache = require("../../network-contracts/artifacts/contracts/chainlinkClient/ENSCacheV2Streamr.sol/ENSCacheV2Streamr.json")
+const ABIstreamRegistry = require("../../network-contracts/artifacts/contracts/StreamRegistry/StreamRegistryV4.sol/StreamRegistryV4.json")
+const namehash = require('eth-ens-namehash')
+const ensAbi = require('@ensdomains/ens/build/contracts/ENS.json')
+
+const {
+    DELAY = "",
+    ENVIRONMENT = "",
+    NETWORK = "",
+    INFURA_API_KEY = "",
+    PRIVATE_KEY = "",
+} = process.env
+let {
+    ENSCacheV2Address = ""
+} = process.env
+const delay = (parseInt(DELAY) || 0) * 1000
+const log = require("debug")("streamr:ens-sync-script")
+let streamRegistryContract: Contract
+let privateKey: string
+let ensCacheContract: Contract
+let ensContract: Contract
+let mainnetProvider: Provider
+let sidechainProvider: Provider
+let mutex = Promise.resolve(true)
+
+async function main(){
+    
+    if (delay) {
+        log(`starting with answer delay ${delay} milliseconds`)
+    }
+
+    let mainnetConfig
+    let sidechainConfig
+    if (ENVIRONMENT === 'prod') {
+        mainnetConfig = Chains.load()["ethereum"]
+        sidechainConfig = Chains.load()["polygon"]
+        mainnetProvider = new InfuraProvider(NETWORK, INFURA_API_KEY)
+        sidechainProvider = new InfuraProvider(NETWORK, INFURA_API_KEY)
+        privateKey = PRIVATE_KEY
+    } else {
+        mainnetConfig = Chains.load()["dev0"]
+        sidechainConfig = Chains.load()["dev1"]
+        mainnetProvider = new JsonRpcProvider(mainnetConfig.rpcEndpoints[0].url)
+        sidechainProvider = new JsonRpcProvider(sidechainConfig.rpcEndpoints[0].url)
+        privateKey = "0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0"
+        ENSCacheV2Address = "0xe78c7E1Ee0fEFAed92758996008F5aB24aa85693"
+    }
+    
+    const ensAddress = mainnetConfig.contracts.ENS
+
+    streamRegistryContract = new Contract(sidechainConfig.contracts.StreamRegistry, ABIstreamRegistry.abi, sidechainProvider)
+    const domainOwnerSidechain = new Wallet(privateKey, sidechainProvider)
+    ensCacheContract = new Contract(ENSCacheV2Address, ABIenscache.abi, domainOwnerSidechain) // TODO
+    ensContract = new Contract(ensAddress, ensAbi.abi, mainnetProvider)
+    log("starting listening for events on ENSCacheV2 contract: ", ensCacheContract.address)
+    ensCacheContract.on("RequestENSOwnerAndCreateStream", async (ensName, streamIdPath, metadataJsonString, requestorAddress) => {
+        log("Got RequestENSOwnerAndCreateStream event params: ", ensName, streamIdPath, metadataJsonString, requestorAddress)
+        if (delay) {
+            log("sleeping for ", delay, "ms")
+            await sleep(delay)
+        }
+        const oldMutex = mutex
+        mutex = new Promise(async (resolve) => {
+            await oldMutex
+            await handleEvent(ensName, streamIdPath, metadataJsonString, requestorAddress)
+            resolve(true)
+        })
+    })
+
+    log("starting listening for createstream events on StreamRegistry contract: ", streamRegistryContract.address)
+    streamRegistryContract.on("StreamCreated", async (streamId, metadataJsonString) => {
+        log("Got StreamCreated event params: ", streamId, metadataJsonString)
+    })
+}
+
+async function handleEvent(ensName: string, streamIdPath: string, metadataJsonString: string, requestorAddress: string) {
+    log("handling event params: ", ensName, streamIdPath, metadataJsonString, requestorAddress)
+    const ensHashedName = namehash.hash(ensName)
+    const owner = await ensContract.owner(ensHashedName)
+    log("ENS owner queried from mainnet: ", owner)
+
+    if (requestorAddress === owner) {
+        await createStream(ensName, streamIdPath, metadataJsonString, requestorAddress, true)
+    } else {
+        log(`Requestor ${requestorAddress} is not owner ${owner}, ignoring request`)
+    }
+}
+
+async function sleep(ms: number) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms)
+    })
+}
+
+async function createStream(ensName: string, streamIdPath: string, metadataJsonString: string, requestorAddress: string, retry = false) {
+
+    if (await streamRegistryContract.exists(ensName + streamIdPath)) {
+        log("stream already exists, not creating")
+        return
+    }
+
+    log("creating stream from ENS name: ", ensName, streamIdPath, metadataJsonString, requestorAddress)
+    try {
+        const tx = await ensCacheContract.fulfillENSOwner(ensName, streamIdPath, metadataJsonString, requestorAddress)
+        await tx.wait()
+        log("createStreamFromENS tx: ", tx.hash)
+    } catch (e) {
+        log("creating stream failed, createStreamFromENS error: ", e)
+        if (retry) {
+            log("retrying")
+            await createStream(ensName, streamIdPath, metadataJsonString, requestorAddress, false)
+        }
+    }
+}
+
+main().then(() => {
+    // debug('done')
+    log("listening for events")
+    return void 0
+}).catch((err: any) => {
+    log("error: ", err)
+    process.exit(1)
+})
