058217655a514e80a90dbec3869facfed3b4496a
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2023-04-18 18:00:38 +0400

    Docker image creation files for dev env

---
+++ packages/ens-sync-script/buildDockerImage.sh
@@ -0,0 +1,6 @@
+mkdir -p network-contracts/artifacts/contracts/StreamRegistry/StreamRegistryV4.sol
+mkdir -p network-contracts/artifacts/contracts/chainlinkClient/ENSCacheV2Streamr.sol
+cp ../network-contracts/artifacts/contracts/StreamRegistry/StreamRegistryV4.sol/StreamRegistryV4.json ./network-contracts/artifacts/contracts/StreamRegistry/StreamRegistryV4.sol
+cp -r ../network-contracts/artifacts/contracts/chainlinkClient/ENSCacheV2Streamr.sol/ENSCacheV2Streamr.json ./network-contracts/artifacts/contracts/chainlinkClient/ENSCacheV2Streamr.sol
+
+docker build --platform linux/AMD64 -t streamr/ens-sync-script:dev .
