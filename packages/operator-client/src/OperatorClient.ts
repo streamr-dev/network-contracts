d0747382a2d4c83937056a5766db180e6b63efe6
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2023-05-25 16:05:47 +0400

    fix: fixed bug with getAllStakedStreams; added more Tests

--- packages/operator-client/src/OperatorClient.ts
+++ packages/operator-client/src/OperatorClient.ts
@@ -58,7 +58,7 @@ export class OperatorClient extends EventEmitter {
             const sponsorshipAddress = sponsorship
             const streamId = await this.getStreamId(sponsorshipAddress)
             if (this.streamIdOfSponsorship.has(sponsorshipAddress)) {
-                console.error("Sponsorship already staked into, in my bookkeeping!")
+                console.info(`Sponsorship ${sponsorship} already staked into, ignoring`)
                 return
             }
             this.streamIdOfSponsorship.set(sponsorshipAddress, streamId)
@@ -94,6 +94,7 @@ export class OperatorClient extends EventEmitter {
     }
 
     async getStakedStreams(): Promise<{ streamIds: string[], blockNumber: number }> {
+        log(`getStakedStreams for ${this.address.toLowerCase()}`)
         const queryResult = await this.theGraphClient.sendQuery({
             // query: `
             //     {
@@ -110,11 +111,12 @@ export class OperatorClient extends EventEmitter {
                 operator(id: "${this.address.toLowerCase()}") {
                   stakes {
                     sponsorship {
+                      id
                       stream {
                         id
                       }
                     }
-                  }
+                 }
                 }
                 _meta {
                     block {
@@ -124,6 +126,14 @@ export class OperatorClient extends EventEmitter {
               }
             `
         })
+        if (!queryResult.operator) {
+            this.logger.error(`Operator ${this.address.toLowerCase()} not found in TheGraph`)
+            return {
+                streamIds: [],
+                // eslint-disable-next-line no-underscore-dangle
+                blockNumber: queryResult._meta.block.number
+            }
+        }
         for (const stake of queryResult.operator.stakes) {
             if (stake.sponsorship.stream && stake.sponsorship.stream.id) {
                 const streamId = stake.sponsorship.stream.id
@@ -133,7 +143,7 @@ export class OperatorClient extends EventEmitter {
             }
         }
         return {
-            streamIds: Array.from(this.streamIdOfSponsorship.values()),
+            streamIds: Array.from(this.sponsorshipCountOfStream.keys()),
             // eslint-disable-next-line no-underscore-dangle
             blockNumber: queryResult._meta.block.number
         }
