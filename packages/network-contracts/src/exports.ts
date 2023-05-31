d46741d6fa0d0bf2c2a4dc320c982890a826cee2
Sami Gabor <samoila.gabor@yahoo.com>
2023-05-31 10:41:24 +0300

    fix: export path

--- packages/network-contracts/src/exports.ts
+++ packages/network-contracts/src/exports.ts
@@ -1,5 +1,5 @@
 export { abi as operatorABI, bytecode as operatorBytecode } from "../artifacts/contracts/OperatorTokenomics/Operator.sol/Operator.json"
-export type { Operator } from "../typechain/contracts/OperatorTokenomics/Operator.sol/Operator"
+export type { Operator } from "../typechain/contracts/OperatorTokenomics/Operator"
 
 export { abi as sponsorshipABI, bytecode as sponsorshipBytecode } from "../artifacts/contracts/OperatorTokenomics/Sponsorship.sol/Sponsorship.json"
 export type { Sponsorship } from "../typechain/contracts/OperatorTokenomics/Sponsorship"
