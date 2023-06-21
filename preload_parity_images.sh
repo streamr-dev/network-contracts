1f7ee5b684d6c11a983f15b6890af176f2a12e21
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2023-06-21 18:57:22 +0400

    fixed chain-predeploy-script

--- preload_parity_images.sh
+++ preload_parity_images.sh
@@ -14,7 +14,7 @@ checkImg poanetwork/omnibridge
 checkImg streamr/tokenbridge-contracts
 
 echo "Building smart-contracts-init docker image"
-docker build -t streamr/smart-contracts-init:dev  -f ./packages/docker-dev-chain-init/Dockerfile .
+docker buildx build --platform linux/AMD64 -t streamr/smart-contracts-init:dev  -f ./packages/docker-dev-chain-init/Dockerfile .
 #echo "Starting Streamr stack to fetch products from Engine and Editor"
 # streamr-docker-dev start
 #echo "Stopping OpenEthereum containers from Streamr stack"
