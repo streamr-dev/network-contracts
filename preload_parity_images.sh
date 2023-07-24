065841510d0ce5c3a9a3c6c3bd3fee203ca92431
Juuso Takalainen <juuso@streamr.network>
2023-07-24 15:53:36 +0300

    build: modify the main build script, delete unused files

--- preload_parity_images.sh
+++ preload_parity_images.sh
@@ -1,32 +1,16 @@
 #!/bin/bash -eux
 cd `dirname $0`
-LOG=smartContractInit.log
 
-echo "Building smart-contracts-init docker image"
-docker buildx build --platform linux/AMD64 -t streamr/smart-contracts-init:dev  -f ./packages/docker-dev-chain-init/Dockerfile .
-#echo "Starting Streamr stack to fetch products from Engine and Editor"
-# streamr-docker-dev start
-#echo "Stopping OpenEthereum containers from Streamr stack"
-# streamr-docker-dev stop parity-sidechain-node0 parity-node0
 cd packages/docker-dev-chain-init
 echo "Starting init stack with OpenEthereum"
 docker-compose up -d parity-sidechain-node0
 echo "waiting 5s for chains to start up"
 sleep 5
-docker-compose up -d smart-contracts-init
-INITSTATUS=`docker wait streamr-dev-smart-contracts-init`
 
-docker logs streamr-dev-smart-contracts-init &> $LOG
-
-# remove timestamps in order to see in diffs if logs have changed
-sed "s/^202[^ ]* //g" $LOG > tmp.log
-cp tmp.log $LOG
-
-echo "streamr-dev-smart-contracts-init finished with status $INITSTATUS. Logs in $LOG"
-test $INITSTATUS -ne 0 && echo "streamr-dev-smart-contracts-init failed" && exit 1
+export DEBUG=*
+npx hardhat run --network localsidechain index.js 2>&1 |tee log.txt
+echo "Snapshotting chain state"
 docker exec streamr-dev-parity-sidechain-node0 /bin/bash -c 'mv /home/parity/parity_data /home/parity/parity_data.default'
-#echo "Stopping Streamr stack"
-#streamr-docker-dev stop
 echo "Committing OpenEthereum images locally"
 docker commit streamr-dev-parity-sidechain-node0 streamr/open-ethereum-poa-sidechain-preload1:dev
 echo "Stopping all docker"
