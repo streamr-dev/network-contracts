3a10f766b8de371b722cf875b8df4b8058e63a7b
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2023-07-28 12:16:00 +0300

    WIP preload fast devchain

--- packages/dev-chain-fast/build_and_preload.sh
+++ packages/dev-chain-fast/build_and_preload.sh
@@ -1,29 +1,17 @@
 #!/bin/bash -eux
-
+LOG=contracDeployment.log
 echo "Creating empty fast-chain docker image"
-build -t streamr/dev-chain-fast .
+docker build -t streamr/dev-chain-fast .
 echo "Deploying contracts"
 
-docker run streamr/dev-chain-fast
-docker logs streamr/dev-chain-fast &> contracDeployment.log
+docker run streamr/dev-chain-fast -p 8545:8545
+docker logs streamr/dev-chain-fast &> $LOG
 
+npm run deploy
 
-# remove timestamps in order to see in diffs if logs have changed
-sed "s/^202[^ ]* //g" $LOG > tmp.log
-cp tmp.log $LOG
+echo "deployment finished, logs in $LOG"
+echo "Committing deployment to image"
 
-echo "streamr-dev-smart-contracts-init finished with status $INITSTATUS. Logs in $LOG"
-test $INITSTATUS -ne 0 && echo "streamr-dev-smart-contracts-init failed" && exit 1
-docker exec streamr-dev-parity-sidechain-node0 /bin/bash -c 'mv /home/parity/parity_data /home/parity/parity_data.default'
-docker exec streamr-dev-parity-node0 /bin/bash -c 'mv /home/parity/parity_data /home/parity/parity_data.default'
-#echo "Stopping Streamr stack"
-#streamr-docker-dev stop
-echo "Committing OpenEthereum images locally"
 docker commit streamr-dev-parity-sidechain-node0 streamr/open-ethereum-poa-sidechain-preload1:dev
-docker commit streamr-dev-parity-node0 streamr/open-ethereum-poa-mainchain-preload1:dev
-echo "Stopping all docker"
-docker-compose stop
-docker-compose rm -f
 echo "Images created. To push to dockerhub: "
-echo docker push streamr/open-ethereum-poa-sidechain-preload1:dev
-echo docker push streamr/open-ethereum-poa-mainchain-preload1:dev
+echo docker push streamr/dev-chain-fast
