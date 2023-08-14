be94986007120020141de1ce6953a1bd83072f51
Eric Andrews <harbu88@hotmail.com>
2023-08-14 11:49:34 +0300

    refactor: simplify @streamr/config package (#594)
    Get rid of convoluted "loading via JSON" and just export config as object directly instead.
    
    The config structure stays the same but some methods of the `Chains` object were removed. Don't know if they were used somewhere, but if they were, this is a breaking change. Those methods were (at least based on tests): `getRPCEndpointsByProtocol` and `Chains#toString`.

--- packages/config/test/index.test.ts
+++ packages/config/test/index.test.ts
@@ -1,104 +1,26 @@
 import { describe, it } from "mocha"
 import { assert } from "chai"
-import * as config from "../src"
+import { config } from "../src"
 
 describe("Load configuration from JSON file", () => {
     it("ethereum chain id is 1", () => {
-        const chains: config.Chains = config.Chains.load()
-        const chainId: number = chains.ethereum.id
+        const chainId: number = config.ethereum.id
         const expected = 1
         assert.equal(chainId, expected, `Expecting ethereum prod chain id to equal ${expected}, got '${chainId}'`)
     })
     it("development chain id is 8995", () => {
-        const chains: config.Chains = config.Chains.load()
-        const chainId: number = chains.dev0.id
+        const chainId: number = config.dev0.id
         const expected = 8995
         assert.equal(chainId, expected, `Expecting ethereum dev chain id to equal ${expected}, got '${chainId}'`)
     })
     it("reads DATA token dev address from JSON", () => {
-        const chains: config.Chains = config.Chains.load()
-        const address = chains.dev0.contracts["DATA"]
+        const address = config.dev0.contracts["DATA"]
         const expected = "0xbAA81A0179015bE47Ad439566374F2Bae098686F"
         assert.equal(address, expected, `Expecting ethereum DATA token to equal ${expected}, got '${address}'`)
     })
     it("reads prod Polygon RPC URL", () => {
-        const chains: config.Chains = config.Chains.load()
-        const rpcHttpUrl = chains.polygon.rpcEndpoints[0].url
+        const rpcHttpUrl = config.polygon.rpcEndpoints[0].url
         const expected = "https://polygon-rpc.com"
         assert.equal(rpcHttpUrl, expected, `Expecting prod polygon RPC URL to equal ${expected}, got '${rpcHttpUrl}'`)
-    })
-    it("finds RPC endpoints by protocol", () => {
-        const chains: config.Chains = config.Chains.load()
-        const endpoints = chains.binance.getRPCEndpointsByProtocol(config.RPCProtocol.HTTP)
-        assert.equal(endpoints.length, 1)
-        assert.equal(endpoints[0].url, "https://bsc-dataseed.binance.org")
-    })
-    it("Chain.toString() returns the name of the chain", () => {
-        const chains: config.Chains = config.Chains.load()
-        const chain = chains["polygon"]
-        assert.equal(chain.toString(), "polygon")
-    })
-    it("Chain.toString() returns the name of the chain in lowercase", () => {
-        const chain = new config.Chain("GNOSIS", 123, new Array<config.RPCEndpoint>(), new config.Contracts())
-        assert.equal(chain.toString(), "gnosis")
-    })
-    describe("Chain constructor", () => {
-        const testCases: {
-            name: string;
-            chain: {
-                name: string;
-                id: number;
-                rpcEndpoints: config.RPCEndpoint[];
-                contracts: config.Contracts;
-            };
-            expectedErrorMessage: string;
-        }[] = [
-            {
-                name: "name cannot be empty",
-                chain: {
-                    name: "",
-                    id: 0,
-                    rpcEndpoints: new Array<config.RPCEndpoint>(),
-                    contracts: new config.Contracts(),
-                },
-                expectedErrorMessage: "Chain name is required",
-            },
-            {
-                name: "id cannot be negative",
-                chain: {
-                    name: "ethereum",
-                    id: -1,
-                    rpcEndpoints: new Array<config.RPCEndpoint>(),
-                    contracts: new config.Contracts(),
-                },
-                expectedErrorMessage: "Chain ID cannot be negative",
-            },
-            {
-                name: "all good",
-                chain: {
-                    name: "ethereum",
-                    id: 1,
-                    rpcEndpoints: new Array<config.RPCEndpoint>(),
-                    contracts: new config.Contracts(),
-                },
-                expectedErrorMessage: "",
-            },
-        ]
-        testCases.forEach((tc) => {
-            it(tc.name, async () => {
-                try {
-                    new config.Chain(tc.chain.name, tc.chain.id, tc.chain.rpcEndpoints, tc.chain.contracts)
-                    if (tc.expectedErrorMessage !== "") {
-                        assert.fail("expecting error")
-                    }
-                } catch (err: any) {
-                    if (tc.expectedErrorMessage !== "") {
-                        assert.equal(err.message, tc.expectedErrorMessage)
-                    } else {
-                        assert.fail(`unexpected error: "${err}"`)
-                    }
-                }
-            })
-        })
     })
 })
