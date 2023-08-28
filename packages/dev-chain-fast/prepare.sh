20e10bc73743b9ec07a8958bebe0ed062e19b147
Juuso Takalainen <juuso@streamr.network>
2023-08-28 11:36:33 +0300

    build: fix prepare.sh

--- packages/dev-chain-fast/prepare.sh
+++ packages/dev-chain-fast/prepare.sh
@@ -9,13 +9,13 @@ cd ../..
 npm run build -w @streamr/network-contracts
 npm run build -w @streamr/hub-contracts
 cd packages/network-contracts
-rm streamr-network-contracts-*.tgz
+rm -f streamr-network-contracts-*.tgz
 npm pack
 cd ../hub-contracts
-rm streamr-hub-contracts-*.tgz
+rm -f streamr-hub-contracts-*.tgz
 npm pack
 cd ../dev-chain-fast
 npm i ../network-contracts/streamr-network-contracts-*.tgz
 npm i ../hub-contracts/streamr-hub-contracts-*.tgz
-rm ../network-contracts/streamr-network-contracts-*.tgz
-rm ../hub-contracts/streamr-hub-contracts-*.tgz
+rm -f ../network-contracts/streamr-network-contracts-*.tgz
+rm -f ../hub-contracts/streamr-hub-contracts-*.tgz
