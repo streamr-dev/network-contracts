9b85947bee0e5c6d77e04dd09c42bc076bd48a60
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2023-09-12 11:44:34 +0300

    added hardhat-contract-sizer to contract build process

--- packages/network-contracts/hardhat.config.ts
+++ packages/network-contracts/hardhat.config.ts
@@ -5,6 +5,7 @@ import "hardhat-ignore-warnings"
 import "solidity-coverage"
 import "hardhat-dependency-compiler"
 import "@nomiclabs/hardhat-etherscan"
+import "hardhat-contract-sizer"
 // import "hardhat-gas-reporter"
 
 import { HardhatUserConfig } from "hardhat/types"
@@ -151,6 +152,12 @@ const config: HardhatUserConfig = {
     },
     mocha: {
         timeout: 100000000
+    },
+    contractSizer: {
+        alphaSort: true,
+        disambiguatePaths: false,
+        runOnCompile: true,
+        strict: false,
     }
 }
 export default config
