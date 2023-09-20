34195f6287160b218852fd216df995dce4c48297
Sami Gabor <45082017+samigabor@users.noreply.github.com>
2023-09-20 12:52:39 +0300

    Preload sample projects in dev env (#641)
    * feat: preload sample projects into the chain
    
    * test: check existing projects in subgraph

---
+++ packages/dev-chain-fast/src/projects.ts
@@ -0,0 +1,77 @@
+export const projects = [
+    {
+        "id": "0x0000000000000000000000000000000000000000000000000000000000000001",
+        "minimumSubscriptionSeconds": 0,
+        "chainIds": [31337],
+        "paymentDetails": [
+            {
+                "beneficiary": "0xa3d1F77ACfF0060F7213D7BF3c7fEC78df847De1",
+                "pricingTokenAddress": "0xAf71Ee871ff1a374F88D6Ff01Cd618cE85127e78",
+                "pricePerSecond": 0
+            }
+        ],
+        "streams": [],
+        "isPublicPurchable": true,
+        "metadata": "{\"isDataUnion\": true}, \"description\": \"payable with TestToken\"}"
+    },
+    {
+        "id": "0x0000000000000000000000000000000000000000000000000000000000000002",
+        "minimumSubscriptionSeconds": 1,
+        "chainIds": [31337],
+        "paymentDetails": [
+            {
+                "beneficiary": "0xAf71Ee871ff1a374F88D6Ff01Cd618cE85127e78",
+                "pricingTokenAddress": "0x3387F44140ea19100232873a5aAf9E46608c791E",
+                "pricePerSecond": 2
+            }
+        ],
+        "streams": [],
+        "isPublicPurchable": true,
+        "metadata": "{\"isDataUnion\": false}, \"description\": \"payable with DATA\"}"
+    },
+    {
+        "id": "0x0000000000000000000000000000000000000000000000000000000000000003",
+        "minimumSubscriptionSeconds": 21,
+        "chainIds": [31337],
+        "paymentDetails": [
+            {
+                "beneficiary": "0xAf71Ee871ff1a374F88D6Ff01Cd618cE85127e78",
+                "pricingTokenAddress": "0x3387F44140ea19100232873a5aAf9E46608c791E",
+                "pricePerSecond": 183643
+            }
+        ],
+        "streams": [],
+        "isPublicPurchable": true,
+        "metadata": "{\"description\": \"payable with DATA\"}"
+    },
+    {
+        "id": "0x0000000000000000000000000000000000000000000000000000000000000004",
+        "minimumSubscriptionSeconds": 1000,
+        "chainIds": [31337],
+        "paymentDetails": [
+            {
+                "beneficiary": "0xAf71Ee871ff1a374F88D6Ff01Cd618cE85127e78",
+                "pricingTokenAddress": "0x3387F44140ea19100232873a5aAf9E46608c791E",
+                "pricePerSecond": 521900
+            }
+        ],
+        "streams": [],
+        "isPublicPurchable": true,
+        "metadata": "string metadata | payable with DATA"
+    },
+    {
+        "id": "0x0000000000000000000000000000000000000000000000000000000000000005",
+        "minimumSubscriptionSeconds": 1000000000,
+        "chainIds": [31337],
+        "paymentDetails": [
+            {
+                "beneficiary": "0xAf71Ee871ff1a374F88D6Ff01Cd618cE85127e78",
+                "pricingTokenAddress": "0x3387F44140ea19100232873a5aAf9E46608c791E",
+                "pricePerSecond": "1000000000000000000"
+            }
+        ],
+        "streams": [],
+        "isPublicPurchable": false,
+        "metadata": "{\"description\": \"payable with DATA\"}"
+    }
+]
--- packages/network-contracts/test/integration/contractsSubgraphSmoketest.test.ts
+++ packages/network-contracts/test/integration/contractsSubgraphSmoketest.test.ts
@@ -22,12 +22,7 @@ describe("docker image integration test", () => {
     })
 
     it("can get all the indexed example data from thegraph", async () => {
-        const resultDynamicIds = await graphClient.queryEntity<{
-        operatorDailyBuckets: [],
-        sponsorshipDailyBuckets: [],
-        stakingEvents: [],
-        sponsoringEvents: []
-     }>({ query: `
+        const resultDynamicIds = await graphClient.queryEntity<any>({ query: `
         {
             operatorDailyBuckets {
                 id
@@ -77,6 +72,19 @@ describe("docker image integration test", () => {
         expect(resultDynamicIds.streams.length).to.equal(3)
     })
 
+    it("can get all the projects from thegraph", async () => {
+        const resultDynamicIds = await graphClient.queryEntity<{ 
+        projects: []
+     }>({ query: `
+        {
+            projects {
+                id
+            }
+        }
+        `})
+        expect(resultDynamicIds.projects.length).to.equal(5)
+    })
+    
     it("can get all the indexed example data from thegraph", async () => {
         const resultDynamicIds = await duGraphClient.queryEntity<{
             dataUnionStatsBuckets: [],
