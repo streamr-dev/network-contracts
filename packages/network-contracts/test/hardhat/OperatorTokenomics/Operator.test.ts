d2acbd15d5b2b98f8ca062fa41734726049749e9
jtakalai <juuso@streamr.network>
2023-09-22 10:43:59 +0300

    Update packages/network-contracts/test/hardhat/OperatorTokenomics/Operator.test.ts
    Co-authored-by: Teo Gebhard <teo@streamr.com>

--- packages/network-contracts/test/hardhat/OperatorTokenomics/Operator.test.ts
+++ packages/network-contracts/test/hardhat/OperatorTokenomics/Operator.test.ts
@@ -245,7 +245,7 @@ describe("Operator contract", (): void => {
     describe("Withdrawing and profit sharing", () => {
 
         // Corresponds to a test in network repo / broker subsystem / operator plugin:
-        // https://github.com/streamr-dev/network/blob/streamr-1.0/packages/broker/test/integration/plugins/operator/maintainOperatorvalue.test.ts
+        // https://github.com/streamr-dev/network/blob/streamr-1.0/packages/broker/test/integration/plugins/operator/maintainOperatorValue.test.ts
         it("can withdraw from sponsorship (happy path)", async function(): Promise<void> {
             const STAKE_AMOUNT = "100"
             const STAKE_AMOUNT_WEI = parseEther(STAKE_AMOUNT)
