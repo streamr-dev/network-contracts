8950fac80efbb22766d2678505798f2c344c9efb
samigabor <samoila.gabor@yahoo.com>
2023-09-25 17:34:25 +0300

    fix: lint unused vars

--- packages/network-contracts/contracts/OperatorTokenomics/testcontracts/TestExchangeRatePolicy.sol
+++ packages/network-contracts/contracts/OperatorTokenomics/testcontracts/TestExchangeRatePolicy.sol
@@ -8,14 +8,14 @@ contract TestExchangeRatePolicy is IExchangeRatePolicy {
 
     function setParam(uint) external {}
 
-    function operatorTokenToData(uint operatorTokenWei) external view returns (uint dataWei) {
+    function operatorTokenToData(uint) external view returns (uint) {
         // solhint-disable-next-line reason-string
         require(false); // using delegatecall the (success, data) returned values will be (0, 0)
     }
 
-    function operatorTokenToDataInverse(uint dataWei) external view returns (uint operatorTokenWei) {}
+    function operatorTokenToDataInverse(uint dataWei) external view returns (uint) {}
 
-    function dataToOperatorToken(uint dataWei, uint alreadyTransferredWei) external view returns (uint operatorTokenWei) {
+    function dataToOperatorToken(uint, uint) external view returns (uint) {
         require(false, "revertedWithStringReason"); // using delegatecall the (success, data) returned values will be (0, 100)
     }
 }
