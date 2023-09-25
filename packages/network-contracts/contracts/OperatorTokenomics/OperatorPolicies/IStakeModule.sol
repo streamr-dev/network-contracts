e96008dc1fae32b5451eb8f5f95e1b4b65f86ffd
Juuso Takalainen <juuso@streamr.network>
2023-09-25 16:25:48 +0300

    fix: update interface

--- packages/network-contracts/contracts/OperatorTokenomics/OperatorPolicies/IStakeModule.sol
+++ packages/network-contracts/contracts/OperatorTokenomics/OperatorPolicies/IStakeModule.sol
@@ -8,7 +8,6 @@ interface IStakeModule {
     function _stake(Sponsorship sponsorship, uint amountWei) external;
     function _reduceStakeTo(Sponsorship sponsorship, uint targetStakeWei) external;
     function _reduceStakeWithoutQueue(Sponsorship sponsorship, uint targetStakeWei) external;
-    function _unstake(Sponsorship sponsorship) external;
     function _unstakeWithoutQueue(Sponsorship sponsorship) external;
     function _forceUnstake(Sponsorship sponsorship, uint maxQueuePayoutIterations) external;
     function _removeSponsorship(Sponsorship sponsorship, uint receivedDuringUnstakingWei) external;
