53217816bbe654855d3c204d1abc9ccc6461e9a1
samigabor <samoila.gabor@yahoo.com>
2023-10-17 11:25:31 +0300

    fix: use DATA tokens for undelegation calculations in the policy

--- packages/network-contracts/contracts/OperatorTokenomics/OperatorPolicies/DefaultUndelegationPolicy.sol
+++ packages/network-contracts/contracts/OperatorTokenomics/OperatorPolicies/DefaultUndelegationPolicy.sol
@@ -20,15 +20,16 @@ contract DefaultUndelegationPolicy is IUndelegationPolicy, Operator {
      * Check the operator's self-undelegation limit i.e. how much of the Operator token supply does the operator have as "skin in the game".
      * For others, it's always OK to undelegate.
      * After self-undelegation, operator must still hold at least minimumSelfDelegationFraction of the total supply.
-     * @dev NOTE that amount can be anything, including more than the actual operator token balance
+     * @dev NOTE that amount can be anything, including more than the actual delegated DATA balance.
      * @dev Consequence of this limit: if there's lots of undelegation queue, those tokens still count for the totalSupply.
      * @dev This means if the queue is left un-serviced, the operator's effective self-delegation limit is higher.
      **/
-    function onUndelegate(address delegator, uint amount) external {
+    function onUndelegate(address delegator, uint amountDataWei) external {
         // limitation only applies to the operator, others can always undelegate
         if (delegator != owner) { return; }
 
-        uint actualAmount = amount < balanceOf(owner) ? amount : balanceOf(owner);
+        uint amountOperatorTokens = moduleCall(address(exchangeRatePolicy), abi.encodeWithSelector(exchangeRatePolicy.operatorTokenToDataInverse.selector, amountDataWei));
+        uint actualAmount = amountOperatorTokens < balanceOf(owner) ? amountOperatorTokens : balanceOf(owner);
         uint balanceAfter = balanceOf(owner) - actualAmount;
         uint totalSupplyAfter = totalSupply() - actualAmount;
         require(1 ether * balanceAfter >= totalSupplyAfter * streamrConfig.minimumSelfDelegationFraction(), "error_selfDelegationTooLow");
