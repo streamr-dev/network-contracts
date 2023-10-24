e2e836c9f880d16333129602736a91ca8123e790
Juuso Takalainen <juuso@streamr.network>
2023-10-24 14:42:58 +0300

    test: update smoke test schema properties

--- packages/network-contracts/test/integration/contractsSubgraphSmoketest.test.ts
+++ packages/network-contracts/test/integration/contractsSubgraphSmoketest.test.ts
@@ -58,7 +58,7 @@ describe("docker image integration test", () => {
                 id
             }
             delegations {
-                delegatedDataWei
+                valueDataWei
                 operatorTokenBalanceWei
             }
         }
@@ -77,7 +77,7 @@ describe("docker image integration test", () => {
         expect(resultDynamicIds.streams.length).to.equal(5)
         expect(resultDynamicIds.streamPermissions.length).to.equal(12) // 3 operators + 9?
 
-        expect(resultDynamicIds.delegations[0].delegatedDataWei).to.equal("5003000000000000000000")
+        expect(resultDynamicIds.delegations[0].valueDataWei).to.equal("5003000000000000000000")
         expect(resultDynamicIds.delegations[0].operatorTokenBalanceWei).to.equal("5003000000000000000000")
     })
 
