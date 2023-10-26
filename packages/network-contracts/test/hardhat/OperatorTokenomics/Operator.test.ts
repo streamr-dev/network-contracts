085b8cf684d6d27790fe0b8cc302c9b791f38a16
Juuso Takalainen <juuso@streamr.network>
2023-10-26 21:03:34 +0300

    test: fix test

--- packages/network-contracts/test/hardhat/OperatorTokenomics/Operator.test.ts
+++ packages/network-contracts/test/hardhat/OperatorTokenomics/Operator.test.ts
@@ -1808,8 +1808,8 @@ describe("Operator contract", (): void => {
         })
 
         it("can call flagging functions", async function(): Promise<void> {
-            // hardhat accounts 1, 2, 3 will be used by setupSponsorships, see "before" hook which they are
-            await setTokens(sponsor, "10000")
+            // hardhat accounts 2, 3, 4 will be used by setupSponsorships, see "before" hook which they are
+            await setTokens(delegator, "10000")
             await setTokens(operatorWallet, "10000")
             await setTokens(operator2Wallet, "10000")
             const {
