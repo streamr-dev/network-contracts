29c10a18c7525ff3e39f684cc61e50fedc54bb5f
Juuso Takalainen <juuso@streamr.network>
2023-10-28 17:32:33 +0300

    test: move test cases under describe block

--- packages/network-contracts/test/hardhat/OperatorTokenomics/Sponsorship.test.ts
+++ packages/network-contracts/test/hardhat/OperatorTokenomics/Sponsorship.test.ts
@@ -54,51 +54,6 @@ describe("Sponsorship contract", (): void => {
         await( await streamrConfig.setFlagReviewerCount(5)).wait()
     })
 
-    it("bad operator (reverts upon transferAndCall) can't prevent getting kicked out of the sponsorship", async function(): Promise<void> {
-        const { token } = contracts
-        const sponsorship = await deploySponsorshipWithoutFactory(contracts)
-        const badOperator = await (await getContractFactory("TestBadOperator", admin)).deploy()
-        await (await token.transferAndCall(sponsorship.address, parseEther("100"), "0x")).wait()
-        await (await token.transfer(badOperator.address, parseEther("100"))).wait()
-        await (await badOperator.stake(sponsorship.address, parseEther("100"), token.address)).wait()
-
-        const sponsorshipBalanceBeforeUnstake = await token.balanceOf(sponsorship.address)
-        const operatorBalanceBeforeUnstake = await token.balanceOf(badOperator.address)
-        const sponsorshipStakeBeforeUnstake = await sponsorship.stakedWei(badOperator.address)
-        await (await badOperator.unstake(sponsorship.address)).wait()
-        const sponsorshipBalanceAfterUnstake = await token.balanceOf(sponsorship.address)
-        const operatorBalanceAfterUnstake = await token.balanceOf(badOperator.address)
-        const sponsorshipStakeAfterUnstake = await sponsorship.stakedWei(badOperator.address)
-
-        expect(sponsorshipStakeBeforeUnstake).to.equal(parseEther("100"))
-        expect(sponsorshipStakeAfterUnstake).to.equal(parseEther("0"))
-        // TestBadOperator.onTokenTransfer reverts so the funds did not get transferred
-        expect(operatorBalanceBeforeUnstake).to.equal(parseEther("0"))
-        expect(operatorBalanceAfterUnstake).to.equal(parseEther("0"))
-        expect(sponsorshipBalanceBeforeUnstake).to.equal(parseEther("200"))
-        expect(sponsorshipBalanceAfterUnstake).to.equal(parseEther("200"))
-
-        // bad operator got kicked out of the sponsorship even though he did not receive the funds from the previous unstake
-        await expect(badOperator.unstake(sponsorship.address))
-            .to.be.revertedWithCustomError(sponsorship, "OperatorNotStaked")
-    })
-
-    it("bad operator (reverts upon transferAndCall) can be kicked out of the sponsorship through admin policy", async function(): Promise<void> {
-        const { token, adminKickPolicy } = contracts
-        const sponsorship = await deploySponsorshipWithoutFactory(contracts, {}, [], [], undefined, undefined, adminKickPolicy)
-        await (await token.transferAndCall(sponsorship.address, parseEther("100"), "0x")).wait()
-        const badOperator = await (await getContractFactory("TestBadOperator", admin)).deploy()
-        await (await token.transfer(badOperator.address, parseEther("100"))).wait()
-        await (await badOperator.stake(sponsorship.address, parseEther("100"), token.address)).wait()
-
-        const badOperatorStakeBeforeKick = await sponsorship.stakedWei(badOperator.address)
-        await expect(sponsorship.connect(admin).flag(badOperator.address, "{}")).to.emit(sponsorship, "OperatorKicked").withArgs(badOperator.address)
-        const badOperatorStakeAfterKick = await sponsorship.stakedWei(badOperator.address)
-
-        expect(badOperatorStakeBeforeKick).to.equal(parseEther("100"))
-        expect(badOperatorStakeAfterKick).to.equal(parseEther("0")) // they're out
-    })
-
     // longer happy path tests
     describe("Scenarios", (): void => {
         it("Multiple stakings and sponsorings", async function(): Promise<void> {
@@ -414,6 +369,52 @@ describe("Sponsorship contract", (): void => {
             // TestKickPolicy actually kicks and slashes given amount (here, 100)
             await expect(sponsorship.voteOnFlag(operator.address, hexZeroPad(parseEther("100").toHexString(), 32)))
                 .to.emit(sponsorship, "OperatorSlashed").withArgs(operator.address, parseEther("70"))
+        })
+
+        it("bad operator (reverts upon transferAndCall) can't prevent getting kicked out of the sponsorship", async function(): Promise<void> {
+            const { token } = contracts
+            const sponsorship = await deploySponsorshipWithoutFactory(contracts)
+            const badOperator = await (await getContractFactory("TestBadOperator", admin)).deploy()
+            await (await token.transferAndCall(sponsorship.address, parseEther("100"), "0x")).wait()
+            await (await token.transfer(badOperator.address, parseEther("100"))).wait()
+            await (await badOperator.stake(sponsorship.address, parseEther("100"), token.address)).wait()
+
+            const sponsorshipBalanceBeforeUnstake = await token.balanceOf(sponsorship.address)
+            const operatorBalanceBeforeUnstake = await token.balanceOf(badOperator.address)
+            const sponsorshipStakeBeforeUnstake = await sponsorship.stakedWei(badOperator.address)
+            await (await badOperator.unstake(sponsorship.address)).wait()
+            const sponsorshipBalanceAfterUnstake = await token.balanceOf(sponsorship.address)
+            const operatorBalanceAfterUnstake = await token.balanceOf(badOperator.address)
+            const sponsorshipStakeAfterUnstake = await sponsorship.stakedWei(badOperator.address)
+
+            expect(sponsorshipStakeBeforeUnstake).to.equal(parseEther("100"))
+            expect(sponsorshipStakeAfterUnstake).to.equal(parseEther("0"))
+            // TestBadOperator.onTokenTransfer reverts so the funds did not get transferred
+            expect(operatorBalanceBeforeUnstake).to.equal(parseEther("0"))
+            expect(operatorBalanceAfterUnstake).to.equal(parseEther("0"))
+            expect(sponsorshipBalanceBeforeUnstake).to.equal(parseEther("200"))
+            expect(sponsorshipBalanceAfterUnstake).to.equal(parseEther("200"))
+
+            // bad operator got kicked out of the sponsorship even though he did not receive the funds from the previous unstake
+            await expect(badOperator.unstake(sponsorship.address))
+                .to.be.revertedWithCustomError(sponsorship, "OperatorNotStaked")
+        })
+
+        it("bad operator (reverts upon transferAndCall) can be kicked out of the sponsorship through admin policy", async function(): Promise<void> {
+            const { token, adminKickPolicy } = contracts
+            const sponsorship = await deploySponsorshipWithoutFactory(contracts, {}, [], [], undefined, undefined, adminKickPolicy)
+            await (await token.transferAndCall(sponsorship.address, parseEther("100"), "0x")).wait()
+            const badOperator = await (await getContractFactory("TestBadOperator", admin)).deploy()
+            await (await token.transfer(badOperator.address, parseEther("100"))).wait()
+            await (await badOperator.stake(sponsorship.address, parseEther("100"), token.address)).wait()
+
+            const badOperatorStakeBeforeKick = await sponsorship.stakedWei(badOperator.address)
+            await expect(sponsorship.connect(admin).flag(badOperator.address, "{}"))
+                .to.emit(sponsorship, "OperatorKicked").withArgs(badOperator.address)
+            const badOperatorStakeAfterKick = await sponsorship.stakedWei(badOperator.address)
+
+            expect(badOperatorStakeBeforeKick).to.equal(parseEther("100"))
+            expect(badOperatorStakeAfterKick).to.equal(parseEther("0")) // they're out
         })
     })
 
