1963099e35ca3cf5c34083408e1842fe1ee33bfb
samigabor <samoila.gabor@yahoo.com>
2023-11-08 13:33:48 +0200

    feat: index all public, non-constant StreamrConfig state variables

---
+++ packages/network-subgraphs/src/streamrConfig.ts
@@ -0,0 +1,80 @@
+import { BigInt, log } from '@graphprotocol/graph-ts'
+import { ConfigChanged } from '../generated/StreamrConfig/StreamrConfig'
+import { Network } from '../generated/schema'
+
+export function handleConfigChanged(event: ConfigChanged): void {
+    let streamrConfigAddress = event.address.toHexString()
+    let key = event.params.key.toString()
+    let newValue = event.params.newValue
+    let newAddress = event.params.newAddress.toHexString()
+
+    log.info('handleConfigChanged: streamrConfigAddress={} key={} newValue={} newAddress={} blockNumber={}', 
+        [streamrConfigAddress, key, newValue.toString(), newAddress, event.block.number.toString()])
+
+    let network = loadOrCreateNetwork(streamrConfigAddress)
+    if (key == "slashingFraction") { network.slashingFraction = newValue }
+    else if (key == "earlyLeaverPenaltyWei") { network.earlyLeaverPenaltyWei = newValue }
+    else if (key == "minimumSelfDelegationFraction") { network.minimumSelfDelegationFraction = newValue }
+    else if (key == "minimumDelegationWei") { network.minimumDelegationWei = newValue }
+    else if (key == "maxPenaltyPeriodSeconds") { network.maxPenaltyPeriodSeconds = newValue }
+    else if (key == "maxQueueSeconds") { network.maxQueueSeconds = newValue }
+    else if (key == "maxAllowedEarningsFraction") { network.maxAllowedEarningsFraction = newValue }
+    else if (key == "fishermanRewardFraction") { network.fishermanRewardFraction = newValue }
+    else if (key == "protocolFeeFraction") { network.protocolFeeFraction = newValue }
+    else if (key == "protocolFeeBeneficiary") { network.protocolFeeBeneficiary = newAddress }
+    else if (key == "minEligibleVoterAge") { network.minEligibleVoterAge = newValue }
+    else if (key == "minEligibleVoterFractionOfAllStake") { network.minEligibleVoterFractionOfAllStake = newValue }
+    else if (key == "flagReviewerCount") { network.flagReviewerCount = newValue }
+    else if (key == "flagReviewerRewardWei") { network.flagReviewerRewardWei = newValue }
+    else if (key == "flaggerRewardWei") { network.flaggerRewardWei = newValue }
+    else if (key == "flagReviewerSelectionIterations") { network.flagReviewerSelectionIterations = newValue }
+    else if (key == "flagStakeWei") { network.flagStakeWei = newValue }
+    else if (key == "reviewPeriodSeconds") { network.reviewPeriodSeconds = newValue }
+    else if (key == "votingPeriodSeconds") { network.votingPeriodSeconds = newValue }
+    else if (key == "flagProtectionSeconds") { network.flagProtectionSeconds = newValue }
+    else if (key == "randomOracle") { network.randomOracle = newAddress }
+    else if (key == "trustedForwarder") { network.trustedForwarder = newAddress }
+    else if (key == "sponsorshipFactory") { network.sponsorshipFactory = newAddress }
+    else if (key == "operatorFactory") { network.operatorFactory = newAddress }
+    else if (key == "voterRegistry") { network.voterRegistry = newAddress }
+    else if (key == "operatorContractOnlyJoinPolicy") { network.operatorContractOnlyJoinPolicy = newAddress }
+    else if (key == "streamRegistryAddress") { network.streamRegistryAddress = newAddress }
+    network.save()
+}
+
+export function loadOrCreateNetwork(id: string): Network {
+    let network = Network.load(id)
+    if (network == null) {
+        network = new Network(id)
+
+        network.slashingFraction = BigInt.zero()
+        network.earlyLeaverPenaltyWei = BigInt.zero()
+        network.minimumDelegationWei = BigInt.zero()
+        network.minimumSelfDelegationFraction = BigInt.zero()
+        network.maxPenaltyPeriodSeconds = BigInt.zero()
+        network.maxQueueSeconds = BigInt.zero()
+        network.maxAllowedEarningsFraction = BigInt.zero()
+        network.fishermanRewardFraction = BigInt.zero()
+        network.protocolFeeFraction = BigInt.zero()
+        network.protocolFeeBeneficiary = ''
+        network.minEligibleVoterAge = BigInt.zero()
+        network.minEligibleVoterFractionOfAllStake = BigInt.zero()
+        network.flagReviewerCount = BigInt.zero()
+        network.flagReviewerRewardWei = BigInt.zero()
+        network.flaggerRewardWei = BigInt.zero()
+        network.flagReviewerSelectionIterations = BigInt.zero()
+        network.flagStakeWei = BigInt.zero()
+        network.reviewPeriodSeconds = BigInt.zero()
+        network.votingPeriodSeconds = BigInt.zero()
+        network.flagProtectionSeconds = BigInt.zero()
+
+        network.randomOracle = ''
+        network.trustedForwarder = ''
+        network.sponsorshipFactory = ''
+        network.operatorFactory = ''
+        network.voterRegistry = ''
+        network.operatorContractOnlyJoinPolicy = ''
+        network.streamRegistryAddress = ''
+    }
+    return network
+}
