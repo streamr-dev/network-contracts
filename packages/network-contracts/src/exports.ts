5ad4016f935cc0f202cee4962dadfbf29b9273e7
jtakalai <juuso@streamr.network>
2023-11-10 15:01:23 +0200

    Export codehashes of contracts (#758)
    * feat: export codehashes of contracts
    
    also write them into a json file, so that they're explicitly available a la selectors.txt
    
    * updated codehases
    
    * feat: revert separate exports file, fix hashing to use deployedBytecode
    
    ---------
    
    Co-authored-by: Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>

--- packages/network-contracts/src/exports.ts
+++ packages/network-contracts/src/exports.ts
@@ -88,4 +88,59 @@ export type { QueueModule } from "../typechain/contracts/OperatorTokenomics/Oper
 export { abi as stakeModuleABI, bytecode as stakeModuleBytecode }
     from "../artifacts/contracts/OperatorTokenomics/OperatorPolicies/StakeModule.sol/StakeModule.json"
 export type { StakeModule } from "../typechain/contracts/OperatorTokenomics/OperatorPolicies/StakeModule"
+
+/* eslint-disable max-len */
+import { deployedBytecode as operatorBytecode } from "../artifacts/contracts/OperatorTokenomics/Operator.sol/Operator.json"
+import { deployedBytecode as sponsorshipBytecode } from "../artifacts/contracts/OperatorTokenomics/Sponsorship.sol/Sponsorship.json"
+import { deployedBytecode as operatorFactoryBytecode } from "../artifacts/contracts/OperatorTokenomics/OperatorFactory.sol/OperatorFactory.json"
+import { deployedBytecode as sponsorshipFactoryBytecode } from "../artifacts/contracts/OperatorTokenomics/SponsorshipFactory.sol/SponsorshipFactory.json"
+import { deployedBytecode as streamrConfigBytecode } from "../artifacts/contracts/OperatorTokenomics/StreamrConfig.sol/StreamrConfig.json"
+import { deployedBytecode as streamRegistryBytecode } from "../artifacts/contracts/StreamRegistry/StreamRegistryV4.sol/StreamRegistryV4.json"
+import { deployedBytecode as streamStorageRegistryBytecode } from "../artifacts/contracts/StreamStorageRegistry/StreamStorageRegistry.sol/StreamStorageRegistry.json"
+import { deployedBytecode as ENSCacheV2Bytecode } from "../artifacts/contracts/chainlinkClient/ENSCacheV2Streamr.sol/ENSCacheV2Streamr.json"
+import { deployedBytecode as nodeRegistryBytecode } from "../artifacts/contracts/NodeRegistry/NodeRegistry.sol/NodeRegistry.json"
+import { deployedBytecode as tokenBytecode } from "../artifacts/contracts/OperatorTokenomics/testcontracts/TestToken.sol/TestToken.json"
+import { deployedBytecode as maxOperatorsJoinPolicyBytecode } from "../artifacts/contracts/OperatorTokenomics/SponsorshipPolicies/MaxOperatorsJoinPolicy.sol/MaxOperatorsJoinPolicy.json"
+import { deployedBytecode as stakeWeightedAllocationPolicyBytecode } from "../artifacts/contracts/OperatorTokenomics/SponsorshipPolicies/StakeWeightedAllocationPolicy.sol/StakeWeightedAllocationPolicy.json"
+import { deployedBytecode as defaultLeavePolicyBytecode } from "../artifacts/contracts/OperatorTokenomics/SponsorshipPolicies/DefaultLeavePolicy.sol/DefaultLeavePolicy.json"
+import { deployedBytecode as voteKickPolicyBytecode } from "../artifacts/contracts/OperatorTokenomics/SponsorshipPolicies/VoteKickPolicy.sol/VoteKickPolicy.json"
+import { deployedBytecode as defaultDelegationPolicyBytecode } from "../artifacts/contracts/OperatorTokenomics/OperatorPolicies/DefaultDelegationPolicy.sol/DefaultDelegationPolicy.json"
+import { deployedBytecode as defaultExchangeRatePolicyBytecode } from "../artifacts/contracts/OperatorTokenomics/OperatorPolicies/DefaultExchangeRatePolicy.sol/DefaultExchangeRatePolicy.json"
+import { deployedBytecode as defaultUndelegationPolicyBytecode } from "../artifacts/contracts/OperatorTokenomics/OperatorPolicies/DefaultUndelegationPolicy.sol/DefaultUndelegationPolicy.json"
+import { deployedBytecode as operatorContractOnlyJoinPolicyBytecode } from "../artifacts/contracts/OperatorTokenomics/SponsorshipPolicies/OperatorContractOnlyJoinPolicy.sol/OperatorContractOnlyJoinPolicy.json"
+import { deployedBytecode as ensRegistryBytecode } from "@ensdomains/ens-contracts/artifacts/contracts/registry/ENSRegistry.sol/ENSRegistry.json"
+import { deployedBytecode as fifsRegistrarBytecode } from "@ensdomains/ens-contracts/artifacts/contracts/registry/FIFSRegistrar.sol/FIFSRegistrar.json"
+import { deployedBytecode as publicResolverBytecode } from "@ensdomains/ens-contracts/deployments/archive/PublicResolver_mainnet_9412610.sol/PublicResolver_mainnet_9412610.json"
+import { deployedBytecode as nodeModuleBytecode } from "../artifacts/contracts/OperatorTokenomics/OperatorPolicies/NodeModule.sol/NodeModule.json"
+import { deployedBytecode as queueModuleBytecode } from "../artifacts/contracts/OperatorTokenomics/OperatorPolicies/QueueModule.sol/QueueModule.json"
+import { deployedBytecode as stakeModuleBytecode } from "../artifacts/contracts/OperatorTokenomics/OperatorPolicies/StakeModule.sol/StakeModule.json"
+/* eslint-enable max-len */
+
+// generate extcodehash for each contract
+import { keccak256 } from "@ethersproject/keccak256"
+export const operatorCodehash = keccak256(operatorBytecode)
+export const sponsorshipCodehash = keccak256(sponsorshipBytecode)
+export const operatorFactoryCodehash = keccak256(operatorFactoryBytecode)
+export const sponsorshipFactoryCodehash = keccak256(sponsorshipFactoryBytecode)
+export const streamrConfigCodehash = keccak256(streamrConfigBytecode)
+export const streamRegistryCodehash = keccak256(streamRegistryBytecode)
+export const streamStorageRegistryCodehash = keccak256(streamStorageRegistryBytecode)
+export const ENSCacheV2Codehash = keccak256(ENSCacheV2Bytecode)
+export const nodeRegistryCodehash = keccak256(nodeRegistryBytecode)
+export const tokenCodehash = keccak256(tokenBytecode)
+export const maxOperatorsJoinPolicyCodehash = keccak256(maxOperatorsJoinPolicyBytecode)
+export const stakeWeightedAllocationPolicyCodehash = keccak256(stakeWeightedAllocationPolicyBytecode)
+export const defaultLeavePolicyCodehash = keccak256(defaultLeavePolicyBytecode)
+export const voteKickPolicyCodehash = keccak256(voteKickPolicyBytecode)
+export const defaultDelegationPolicyCodehash = keccak256(defaultDelegationPolicyBytecode)
+export const defaultExchangeRatePolicyCodehash = keccak256(defaultExchangeRatePolicyBytecode)
+export const defaultUndelegationPolicyCodehash = keccak256(defaultUndelegationPolicyBytecode)
+export const operatorContractOnlyJoinPolicyCodehash = keccak256(operatorContractOnlyJoinPolicyBytecode)
+export const ensRegistryCodehash = keccak256(ensRegistryBytecode)
+export const fifsRegistrarCodehash = keccak256(fifsRegistrarBytecode)
+export const publicResolverCodehash = keccak256(publicResolverBytecode)
+export const nodeModuleCodehash = keccak256(nodeModuleBytecode)
+export const queueModuleCodehash = keccak256(queueModuleBytecode)
+export const stakeModuleCodehash = keccak256(stakeModuleBytecode)
+
 export * from "./StreamrEnvDeployer"
