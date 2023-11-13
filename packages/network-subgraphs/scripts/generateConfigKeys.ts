9973c8352f7ef96aa35ff0e2d64dce42e177c057
samigabor <samoila.gabor@yahoo.com>
2023-11-13 16:33:23 +0200

    feat: add script to generate hashes from strings
    indexed string params are hashed before emitted and there is no way to know what was the emitted string
    This script indexes the StreamrConfig keys to be compared in assemblyscript

---
+++ packages/network-subgraphs/scripts/generateConfigKeys.ts
@@ -0,0 +1,41 @@
+import {ethers} from "ethers";
+
+const configKeys = [
+    "slashingFraction",
+    "earlyLeaverPenaltyWei",
+    "minimumSelfDelegationFraction",
+    "minimumDelegationWei",
+    "maxPenaltyPeriodSeconds",
+    "maxQueueSeconds",
+    "maxAllowedEarningsFraction",
+    "fishermanRewardFraction",
+    "protocolFeeFraction",
+    "protocolFeeBeneficiary",
+    "minEligibleVoterAge",
+    "minEligibleVoterFractionOfAllStake",
+    "flagReviewerCount",
+    "flagReviewerRewardWei",
+    "flaggerRewardWei",
+    "flagReviewerSelectionIterations",
+    "flagStakeWei",
+    "reviewPeriodSeconds",
+    "votingPeriodSeconds",
+    "flagProtectionSeconds",
+    "randomOracle",
+    "trustedForwarder",
+    "sponsorshipFactory",
+    "operatorFactory",
+    "voterRegistry",
+    "operatorContractOnlyJoinPolicy",
+    "streamRegistryAddress",
+];
+
+let lines = ""
+
+for (const key of configKeys) {
+    const value = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(key));
+    lines += `const ${key}Key: string = "${value}"\n`;
+    
+}
+
+console.log(lines)
