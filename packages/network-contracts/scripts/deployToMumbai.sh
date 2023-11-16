01ce726b1968544cb7450862d493b0c4c392766a
Juuso Takalainen <juuso@streamr.network>
2023-11-16 12:34:41 +0200

    refactor: rename script

--- packages/network-contracts/scripts/deployToMumbai.sh
+++ packages/network-contracts/scripts/deployToMumbai.sh
@@ -5,7 +5,7 @@ export CHAIN=mumbai
 export IGNORE_TOKEN_SYMBOL=1
 npm run deployTokenomicsContracts
 
-export TARGET_ADDRESS=0x63f74A64fd334122aB5D29760C6E72Fb4b752208
+export NEW_ADMIN_ADDRESS=0x63f74A64fd334122aB5D29760C6E72Fb4b752208
 export SKIP_REVOKE_CONFIGURATOR=1
-export SCRIPT_FILE=scripts/handoverTo.ts
+export SCRIPT_FILE=scripts/handOverAdminRolesTo.ts
 npm run hardhatScript
