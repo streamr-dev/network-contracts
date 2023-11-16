32ad41f9afd6f66a274b7d795dca364bae57404a
Juuso Takalainen <juuso@streamr.network>
2023-11-16 14:11:22 +0200

    fix: remove skip revoke

--- packages/network-contracts/scripts/deployToPolygon.sh
+++ packages/network-contracts/scripts/deployToPolygon.sh
@@ -5,6 +5,5 @@ export CHAIN=polygon
 npm run deployTokenomicsContracts
 
 export NEW_ADMIN_ADDRESS=0x63f74A64fd334122aB5D29760C6E72Fb4b752208
-export SKIP_REVOKE_CONFIGURATOR=1
 export SCRIPT_FILE=scripts/handOverAdminRolesTo.ts
 npm run hardhatScript
