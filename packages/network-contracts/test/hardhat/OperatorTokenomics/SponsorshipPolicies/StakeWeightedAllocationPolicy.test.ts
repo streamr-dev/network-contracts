2ce3cbca8ee8c8d7859b71bb48f0e2c4d1a3a241
Juuso Takalainen <juuso@streamr.network>
2023-11-23 13:05:20 +0200

    test: add to test also money running out

--- packages/network-contracts/test/hardhat/OperatorTokenomics/SponsorshipPolicies/StakeWeightedAllocationPolicy.test.ts
+++ packages/network-contracts/test/hardhat/OperatorTokenomics/SponsorshipPolicies/StakeWeightedAllocationPolicy.test.ts
@@ -738,21 +738,23 @@ describe("StakeWeightedAllocationPolicy", (): void => {
     })
 
     it("allocates correctly when number of operators is below minOperatorCount at times", async function(): Promise<void> {
-        // t0            : operator1 joins, stakes 500 tokens
-        // t1 = t0 + 1200: operator2 joins, stakes 500 tokens // sponsorship starts paying out since minOperatorCount >= 2
-        // t2 = t0 + 2400: operator3 joins, stakes 500 tokens
-        // t3 = t0 + 3600: operator1 leaves
-        // t4 = t0 + 4800: operator2 leaves // sponsorship stops paying out since minOperatorCount < 2
-        // t5 = t0 + 6000: operator3 leaves
-        //                        t0    t1    t2    t3     t4    t5
-        // operator1 has earned   0     0 + 600 + 400 +    0 +   0 = 1000
-        // operator2 has earned   0     0 + 600 + 400 +  600 +   0 = 1600
-        // operator3 has earned   0     0 +   0 + 400 +  600 +   0 = 1000
+        // t0            : operator1 joins, stakes 10000 tokens
+        // t1 = t0 + 1000: operator2 joins, stakes 10000 tokens, sponsorship starts paying out since operator count >= 2
+        // t2 = t0 + 2000: operator1 leaves, sponsorship no longer running since operator count < 2
+        // t3 = t0 + 3000: operator3 joins, stakes 10000 tokens, sponsorship is again running since operator count >= 2
+        // t4 = t0 + 4000: sponsorship runs out
+        // t5 = t0 + 5000: operator3 leaves, sponsorship no longer running since operator count < 2
+        // t6 = t0 + 6000: top-up, sponsorship would resume paying but it's not running
+        // t7 = t0 + 7000: operator2 leaves
+        //               t0 ... t1 ... t2 ... t3 ... t4 ... t5 ... t6 ... t7
+        // operator1 earns   0   +  500                                    =  500
+        // operator2 earns          500 +    0 +  500 +    0 +    0 +    0 = 1000
+        // operator3 earns                        500 +    0               =  500
         const { token } = contracts
         const sponsorship = await deploySponsorshipWithoutFactory(contracts, {
             minOperatorCount: 2
         })
-        await (await sponsorship.sponsor(parseEther("5000"))).wait()
+        await (await sponsorship.sponsor(parseEther("2000"))).wait()
 
         const tokensOperator1Before = await token.balanceOf(operator.address)
         const tokensOperator2Before = await token.balanceOf(operator2.address)
@@ -762,39 +764,52 @@ describe("StakeWeightedAllocationPolicy", (): void => {
         expect(await sponsorship.isRunning()).to.be.false
 
         await advanceToTimestamp(timeAtStart, "Operator 1 joins")
-        await (await token.connect(operator).transferAndCall(sponsorship.address, parseEther("500"), operator.address)).wait()
+        await (await token.connect(operator).transferAndCall(sponsorship.address, parseEther("10000"), operator.address)).wait()
         expect(await sponsorship.isRunning()).to.be.false
+        expect(await sponsorship.isFunded()).to.be.true
 
-        await advanceToTimestamp(timeAtStart + 1200, "Operator 2 joins")
-        await (await token.connect(operator2).transferAndCall(sponsorship.address, parseEther("500"), operator2.address)).wait()
+        await advanceToTimestamp(timeAtStart + 1000, "Operator 2 joins")
+        await (await token.connect(operator2).transferAndCall(sponsorship.address, parseEther("10000"), operator2.address)).wait()
         expect(await sponsorship.isRunning()).to.be.true
+        expect(await sponsorship.isFunded()).to.be.true
 
-        await advanceToTimestamp(timeAtStart + 2400, "Operator 3 joins")
-        await (await token.connect(operator3).transferAndCall(sponsorship.address, parseEther("500"), operator3.address)).wait()
-        expect(await sponsorship.isRunning()).to.be.true
-
-        await advanceToTimestamp(timeAtStart + 3600, "Operator 1 leaves")
+        await advanceToTimestamp(timeAtStart + 2000, "Operator 1 leaves")
         await (await sponsorship.connect(operator).unstake()).wait()
-        expect(await sponsorship.isRunning()).to.be.true
-
-        await advanceToTimestamp(timeAtStart + 4800, "Operator 2 leaves")
-        await (await sponsorship.connect(operator2).unstake()).wait()
         expect(await sponsorship.isRunning()).to.be.false
+        expect(await sponsorship.isFunded()).to.be.true
 
-        await advanceToTimestamp(timeAtStart + 6000, "Operator 3 leaves")
+        await advanceToTimestamp(timeAtStart + 3000, "Operator 3 joins")
+        await (await token.connect(operator3).transferAndCall(sponsorship.address, parseEther("10000"), operator3.address)).wait()
+        expect(await sponsorship.isRunning()).to.be.true
+        expect(await sponsorship.isFunded()).to.be.true
+
+        // timeAtStart + 4001: sponsorship runs out (+1 because all tx happen one second "late" in test env)
+
+        await advanceToTimestamp(timeAtStart + 5000, "Operator 3 leaves")
         await (await sponsorship.connect(operator3).unstake()).wait()
         expect(await sponsorship.isRunning()).to.be.false
+        expect(await sponsorship.isFunded()).to.be.false
+
+        await advanceToTimestamp(timeAtStart + 6000, "Money is added")
+        await (await sponsorship.sponsor(parseEther("10000"))).wait()
+        expect(await sponsorship.isRunning()).to.be.false
+        expect(await sponsorship.isFunded()).to.be.true
+
+        await advanceToTimestamp(timeAtStart + 7000, "Operator 2 leaves")
+        await (await sponsorship.connect(operator2).unstake()).wait()
+        expect(await sponsorship.isRunning()).to.be.false
+        expect(await sponsorship.isFunded()).to.be.true
 
         const tokensOperator1 = (await token.balanceOf(operator.address)).sub(tokensOperator1Before)
         const tokensOperator2 = (await token.balanceOf(operator2.address)).sub(tokensOperator2Before)
         const tokensOperator3 = (await token.balanceOf(operator3.address)).sub(tokensOperator3Before)
 
-        expect(tokensOperator1).to.equal(parseEther("1000.0"))
-        expect(tokensOperator2).to.equal(parseEther("1600.0"))
-        expect(tokensOperator3).to.equal(parseEther("1000.0"))
+        expect(tokensOperator1).to.equal(parseEther("500.0"))
+        expect(tokensOperator2).to.equal(parseEther("1000.0"))
+        expect(tokensOperator3).to.equal(parseEther("500.0"))
 
-        // 5000 + 500 + 500 + 500 - (1000 + 500) - (1600 + 500) - (1000 + 500) = 1400
-        expect(await token.balanceOf(sponsorship.address)).to.equal(parseEther("1400.0"))
+        // the 10000 added though never paid out...
+        expect(await token.balanceOf(sponsorship.address)).to.equal(parseEther("10000.0"))
     })
 
     it("gets allocation 0 from unjoined operator", async function(): Promise<void> {
