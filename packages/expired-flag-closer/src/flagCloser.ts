03eb53170d8033007a1342ed204d793b4dfb4c0b
Juuso Takalainen <juuso@streamr.network>
2023-12-05 15:57:21 +0200

    feat: show network

--- packages/expired-flag-closer/src/flagCloser.ts
+++ packages/expired-flag-closer/src/flagCloser.ts
@@ -15,7 +15,7 @@ const {
     INTERVALSEC
 } = process.env
 
-const flagLifetime = 60 * 75 // 75 minutes
+const flagLifetimeSeconds = 60 * 75 // 75 minutes
 
 if (!ENV || !(config as {[index: string]: any})[ENV]) {
     throw new Error(`Unknown ENV: ${ENV}`)
@@ -28,12 +28,13 @@ const graphClient = new TheGraphClient({
     fetch,
     logger: new Logger(module)
 })
-const provider = new JsonRpcProvider(ETHEREUM_RPC_URL || envConfig.rpcEndpoints[0].url)
+const rpcUrl = ETHEREUM_RPC_URL || envConfig.rpcEndpoints[0].url
+const provider = new JsonRpcProvider(rpcUrl)
 const signer = new Wallet(PRIVKEY || "", provider).connect(provider)
 
 async function checkForFlags() {
-    console.log('checking, flag lifetime is', flagLifetime, 'seconds')
-    const minFlagStartTime = Math.floor(Date.now() / 1000) - flagLifetime
+    console.log('checking, flag lifetime is %d seconds', flagLifetimeSeconds)
+    const minFlagStartTime = Math.floor(Date.now() / 1000) - flagLifetimeSeconds
     // console.log('min flag start time', minFlagStartTime)
     // const minFlagStartTime = Math.floor(Date.now() / 1000)
     let flags: any
@@ -59,32 +60,29 @@ async function checkForFlags() {
     console.log('found', flags.flags.length, 'flags')
     for (const flag of flags.flags) {
         // console.log('flag', flag)
-        const flagId = flag.id
+        const flagID = flag.id
         const operatorAddress = flag.target.id
         const sponsorship = flag.sponsorship.id
         const sponsorshipContract = new Contract(sponsorship, sponsorshipABI, signer) as unknown as Sponsorship
         // console.log('flag timestamp', flag.flaggingTimestamp, 'min flag age', minFlagStartTime)
         if (flag.flaggingTimestamp < minFlagStartTime) {
-            await endFlag(flagId, sponsorshipContract, operatorAddress)
+            try {
+                console.log('flag id:', flagID, 'sending close flag tx')
+                const tx = await sponsorshipContract.voteOnFlag(operatorAddress, randomBytes(32))
+                console.log('flag id:', flagID, 'sent tx, tx hash: ', tx.hash)
+                const receipt = await tx.wait()
+                console.log('flag id:', flagID, 'tx mined', receipt.transactionHash)
+            } catch (e) {
+                console.log('flag id:', flagID, 'failed to send tx', e)
+            }
         }
     }
 }
 
 async function main() {
+    console.log('Connected to %s, %o', rpcUrl, await provider.getNetwork())
     await checkForFlags()
     setInterval(checkForFlags, parseInt(INTERVALSEC || "900") * 1000) // default 15 minutes
 }
 
 main().catch(console.error)
-
-const endFlag = async (flagID: string, sponsorshipContract: Sponsorship, operatorAddress: string) => {
-    try {
-        console.log('flag id:', flagID, 'sending close flag tx')
-        const tx = await sponsorshipContract.voteOnFlag(operatorAddress, randomBytes(32))
-        console.log('flag id:', flagID, 'sent tx, tx hash: ', tx.hash)
-        const receipt = await tx.wait()
-        console.log('flag id:', flagID, 'tx mined', receipt.transactionHash)
-    } catch (e) {
-        console.log('flag id:', flagID, 'failed to send tx', e)
-    }
-}
