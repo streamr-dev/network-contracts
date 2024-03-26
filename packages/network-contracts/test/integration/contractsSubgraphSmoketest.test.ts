889dd35785e41e4300aae27dfee50ab1c101acf8
Juuso Takalainen <juuso@streamr.network>
2024-03-26 12:15:00 +0200

    test: fix
    operator contract owner is different for the different contracts, don't assert it

--- packages/network-contracts/test/integration/contractsSubgraphSmoketest.test.ts
+++ packages/network-contracts/test/integration/contractsSubgraphSmoketest.test.ts
@@ -104,8 +104,8 @@ describe("docker image integration test", () => {
 
         resultDynamicIds.operators.forEach((operator: any) => {
             expect(operator.controllers.length).to.equal(2)
-            expect(operator.controllers[0]).to.equal("0xa3d1F77ACfF0060F7213D7BF3c7fEC78df847De1")
-            expect(operator.controllers[0]).to.equal("0xa6743286b55f36afa5f4e7e35b6a80039c452dbd")
+            // controller #0 is the operator owner themselves
+            expect(operator.controllers[1]).to.equal("0xa6743286b55f36afa5f4e7e35b6a80039c452dbd")
         })
 
         // 3 operator coordination streams, each has 3 permissions (public + owner + ?)
