b46c3faafa8944d3cbe0ab834269e05c3f9ab25e
Juuso Takalainen <juuso@streamr.network>
2024-08-23 16:43:30 +0300

    build: fix copy-paste hazard

--- packages/hub-contracts/build.sh
+++ packages/hub-contracts/build.sh
@@ -3,7 +3,6 @@ set -ex
 
 rm -rf artifacts
 hardhat compile
-cp  ../../node_modules/@ensdomains/ens-contracts/deployments/archive/PublicResolver_mainnet_9412610.sol/PublicResolver_mainnet_9412610.json artifacts/PublicResolver_mainnet_9412610.json
 
 sleep 1
 
