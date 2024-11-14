2f55ad76ad2ace0b7088e735ed6516e2551882c6
Juuso Takalainen <juuso@streamr.network>
2024-11-14 11:33:38 +0200

    remove non StreamRegistryV5 related scripts from PR

--- packages/network-contracts/scripts/2024-05-03-deployToAmoy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-export CHAIN=polygonAmoy
-export SCRIPT_FILE=scripts/deployStreamrContracts.ts
-npm run hardhatScript
-
-export SCRIPT_FILE=scripts/deployTokenomicsContracts.ts
-npm run hardhatScript
