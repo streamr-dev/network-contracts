ca75ad54538d9fa73f65e4604208920bc03afe49
Juuso Takalainen <juuso@streamr.network>
2025-01-17 17:23:12 +0200

    build: remove unnecessary task

--- packages/npm-network-contracts/build.sh
+++ packages/npm-network-contracts/build.sh
@@ -4,7 +4,7 @@ set -ex
 rm -rf contracts artifacts typechain
 cp -r ../network-contracts/contracts .
 
-npm run compile
+hardhat compile
 
 # copy the artifact file for deployed PublicResolver to avoid npm dependency (the file is NOT going to change or update!)
 cp  ./node_modules/@ensdomains/ens-contracts/deployments/archive/PublicResolver_mainnet_9412610.sol/PublicResolver_mainnet_9412610.json artifacts/PublicResolver_mainnet_9412610.json
