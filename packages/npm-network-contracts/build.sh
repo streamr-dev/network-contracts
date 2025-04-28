30f69b4ae94d9cd2ebad1f72e0d085d19fc3527e
Teo Gebhard <teo@streamr.com>
2025-04-28 10:34:28 +0300

    minify ABIs

--- packages/npm-network-contracts/build.sh
+++ packages/npm-network-contracts/build.sh
@@ -11,3 +11,11 @@ cp  ./node_modules/@ensdomains/ens-contracts/deployments/archive/PublicResolver_
 
 rm -rf dist
 tsc
+
+# create minified ABIs
+artifact_files=$(find artifacts/contracts -type f -name '*.json' ! -name '*.dbg.json' | grep -v 'build-info')
+for artifact_file in $artifact_files; do
+    abi_file="$(dirname "$artifact_file")/$(basename "$artifact_file" .json)ABI.json"
+    jq '.abi' "$artifact_file" > "$abi_file"
+    node minify-abi.mjs "$abi_file"
+done
