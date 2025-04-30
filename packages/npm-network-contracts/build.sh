915231c838f0a7e672434bbb11921ec48c162a95
Juuso Takalainen <juuso@streamr.network>
2025-04-30 11:39:33 +0300

    build: more readable append

--- packages/npm-network-contracts/build.sh
+++ packages/npm-network-contracts/build.sh
@@ -7,9 +7,11 @@ cp -r ../network-contracts/contracts .
 hardhat compile
 
 # create minified ABIs
-artifact_files=$(find artifacts/contracts -type f -name '*.json' ! -name '*.dbg.json')
-artifact_files="${artifact_files}
-artifacts/@streamr/data-v2/flattened/DATAv2.sol/DATAv2.json"
+artifact_files=$(
+    find artifacts/contracts -type f -name '*.json' ! -name '*.dbg.json' &&
+    echo "artifacts/@streamr/data-v2/flattened/DATAv2.sol/DATAv2.json"
+)
+
 for artifact_file in $artifact_files; do
     abi_file="$(echo "$artifact_file" | sed 's#^artifacts/#abis/#')"
     mkdir -p "$(dirname "$abi_file")"
