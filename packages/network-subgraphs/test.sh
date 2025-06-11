de15ec478efc266ce577d92d2294d8b0edcad316
Teo Gebhard <teo@streamr.com>
2025-06-11 14:34:33 +0300

    rm temporary tools

--- packages/network-subgraphs/test.sh
+++
@@ -1,7 +0,0 @@
-node create-subgraph-config-json.mjs > subgraph-config.json
- ./generate-yamls.sh
-diff subgraph_amoy.yaml subgraph_amoy_generated.yaml
-diff subgraph_dev2.yaml subgraph_dev2_generated.yaml
-diff subgraph_iotex.yaml subgraph_iotext_generated.yaml
-diff subgraph_peaq.yaml subgraph_peaq_generated.yaml
-diff subgraph_polygon.yaml subgraph_polygon_generated.yaml
