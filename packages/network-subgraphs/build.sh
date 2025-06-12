b575045bfc5eac07bbc741158545c99248ec09d4
Teo Gebhard <teo@streamr.com>
2025-06-12 20:54:00 +0300

    chore(network-subgraphs): Template for `subgraph.yaml` (#1016)
    Extracted subgraph configs (mainly contract addresses and `startBlock`
    values) to a separate `subgraph-config.json` file. Now the actual
    `subgraph.yaml` is generated in `build.sh` by filling the config options
    to a Handlebars template.
    
    We don't need separate environment specific `subgraph_* yaml` files
    anymore.
    
    ## Implementation
    
    There is a small `fill-handlebars-template.mjs` script which reads the
    config from `stdin` and outputs the filled template to `stdout`. There
    is no custom logic in that script, and alternative we could use some
    ready-made external Handlebars utility instead of this script.
    
    ## Config file
    
    The config file was generated from the existing `subgraph_*.yaml` files
    by this script:
    
    ```js
    import { readFileSync } from 'fs'
    import yaml from 'js-yaml'
    
    const ENVIRONMENT_IDS = ['dev2', 'polygon', 'polygonAmoy', 'peaq', 'iotex']
    
    const output = {}
    
    for (const environmentId of ENVIRONMENT_IDS) {
        const fileId = (environmentId === 'polygonAmoy') ? 'amoy' : environmentId
        const fileName = `subgraph_${fileId}.yaml`
        const fileContents = readFileSync(fileName, 'utf8')
        const yamlData = yaml.load(fileContents)
        const contracts = {}
        for (const dataSource of yamlData.dataSources) {
            const source = dataSource.source
            const key = (source.abi === 'NodeRegistry') ? 'StorageNodeRegistry' : source.abi
            contracts[key] = {
                address: source.address,
                startBlock: source.startBlock
            }
        }
        const networkId = (environmentId === 'polygon') 
            ? 'matic' : 
                ((environmentId === 'polygonAmoy') ? 'polygon-amoy' : environmentId)
        output[environmentId] = {
            networkId,
            hubContracts: (environmentId !== 'iotex'),
            contracts
        }
    }
    
    console.log(JSON.stringify(output, undefined, 4))
    ```
    
    ## Small changes to subgraph.yaml output
    
    The subgraph.yaml output generated by the `build.sh` matches almost
    exactly the current files. Exceptions:
    - removed some TODO comments
    - unified quotes to be single quotes
    - unified the description
    
    See three commits in this PR for the exact unifications. There was also
    test.sh script in commit
    [bda37c6](https://github.com/streamr-dev/network-contracts/pull/1016/commits/bda37c6e92792ef527011b2672bede922c224e8e)
    to check that the outputs match the current files.
    
    ## Notes about Iotex
    
    Thet addresses in the `subgraph-config.json` matches the addresses which
    were used in `subgraph_iotex.yaml`, but don't match the values of
    `@streamr/config`. Therefore this PR doesn't change any functionality.
    There is a separate PR about this
    https://github.com/streamr-dev/network-contracts/pull/1010 issue.
    
    Also in that PR or in a follow-up PR we may add hub contracts to `iotex`
    config. If we do that we should remove the `hubContracts` property from
    the `subgraph-configs.json` (and the related if statement in
    `subgraph.yaml.hbs`) as the value for that is true in environment.s

--- packages/network-subgraphs/build.sh
+++ packages/network-subgraphs/build.sh
@@ -7,7 +7,7 @@ fi
 
 # TODO could build these to "dist" directory so that we don't need to clean up the subgraph.yaml in package.json (the "rm subgraph.yaml" command)
 # - then we can also remove subgraph.yaml from .gitignore
-cp subgraph_$1.yaml subgraph.yaml 
+jq '.'$1 subgraph-config.json | node scripts/fill-handlebars-template.mjs subgraph.yaml.hbs > subgraph.yaml
 ./scripts/copyAbisFromContractsPackage.sh
 npx graph codegen
 npx graph build
