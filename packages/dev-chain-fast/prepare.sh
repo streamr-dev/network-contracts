b81f243c1caca3c8404fe7dcfae351b583da63a9
Teo Gebhard <teo@streamr.com>
2025-06-18 11:33:43 +0300

    rm hub-contracts from dev-chain-fast dependency and preparebuild

--- packages/dev-chain-fast/prepare.sh
+++ packages/dev-chain-fast/prepare.sh
@@ -6,15 +6,10 @@ cd "$(dirname "$0")"
 
 npm run clean
 rm -f streamr-network-contracts-*.tgz
-rm -f streamr-hub-contracts-*.tgz
 cd ../..
 npm run build -w @streamr/network-contracts
-npm run build -w @streamr/hub-contracts
 cd packages/network-contracts
 npm pack
 mv streamr-network-contracts-*.tgz ../dev-chain-fast
-cd ../hub-contracts
-npm pack
-mv streamr-hub-contracts-*.tgz ../dev-chain-fast
 cd ../dev-chain-fast
 npm run build
